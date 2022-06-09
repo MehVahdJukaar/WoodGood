b0a1ba4ebd3a6a1af6f7a5e1a9a09c8a398ea2c5
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-06-09 22:26:48 +0200

    re added main changes

--- src/main/java/net/mehvahdjukaar/every_compat/WoodGood.java
+++ src/main/java/net/mehvahdjukaar/every_compat/WoodGood.java
@@ -1,8 +1,10 @@
 package net.mehvahdjukaar.every_compat;
 
 
-import net.mehvahdjukaar.every_compat.configs.EarlyConfigs;
+import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.every_compat.configs.BlockTypeEnabledCondition;
+import net.mehvahdjukaar.every_compat.configs.EarlyConfigs;
 import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
 import net.mehvahdjukaar.every_compat.dynamicpack.ServerDynamicResourcesHandler;
 import net.mehvahdjukaar.every_compat.misc.CustomRecipeLoader;
@@ -39,12 +41,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -62,12 +65,12 @@ public class WoodGood {
 
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public static final List<CompatModule> ACTIVE_MODULES = new ArrayList<>();
+    public static final Map<String, CompatModule> ACTIVE_MODULES = new LinkedHashMap<>();
 
-    public static final List<CompatMod> COMPETITOR_MODS = new ArrayList<>();
+    public static final List<CompatMod> COMPAT_MODS = new ArrayList<>();
 
     public static void forAllModules(Consumer<CompatModule> action) {
-        ACTIVE_MODULES.forEach(action);
+        ACTIVE_MODULES.values().forEach(action);
     }
 
     public static ServerDynamicResourcesHandler SERVER_RESOURCES = null;
@@ -78,12 +81,17 @@ public class WoodGood {
     public WoodGood() {
 
         //addCompetitorMod("much_more_mod_compat");
-        addCompetitorMod("compatoplenty", List.of("biomesoplenty"));
-        addCompetitorMod("decorative_compat", List.of("biomesoplenty"));
-        addCompetitorMod("compat_makeover", List.of("biomemakeover"));
-        addCompetitorMod("blocksplus", List.of("quark"));
+        addCompetitorMod("compatoplenty", "biomesoplenty", List.of("twigs", "farmersdelight", "quark"));
+        addCompetitorMod("compat_makeover", "biomemakeover", List.of("habitat", "farmersdelight", "quark", "decorative_blocks"));
+        addCompetitorMod("decorative_compat", "biomesoplenty", List.of("decorative_blocks"));
+        addCompetitorMod("macawsbridgesbop", "mcwbridges", List.of("biomesoplenty"));
+        addCompetitorMod("macawbridgesbyg", "mcwbridges", List.of("byg"));
+
+        addCompetitorMod("mcwfencesbop", "mcwfences", List.of("biomesoplenty"));
+        addCompetitorMod("mcwfencesbyg", "mcwfences", List.of("byg"));
 
 
+        /*
         addModule("another_furniture", () -> AnotherFurnitureModule::new);
         addModule("backpacked", () -> BackpackedModule::new);
         addModule("farmersdelight", () -> FarmersDelightModule::new);
@@ -93,18 +101,21 @@ public class WoodGood {
         addModule("create", () -> CreateModule::new);
         addModule("twilightforest", () -> TwilightForestModule::new);
         addModule("valhelsia_structures", () -> ValhelsiaStructuresModule::new);
-        addModule("mcwbridges", () -> MacawBridgesModule::new);
-        addModule("mcwdoors", () -> MacawDoorsModule::new);
+        addModule("cfm", () -> MrCrayfishFurnitureModule::new);
+        */
+        addModule("quark", () -> QuarkModule::new);
+        // addModule("quark", () -> LegacyQM::new);
+        addModule("mcwwindows", () -> MacawWindowsModule::new);
         addModule("mcwfences", () -> MacawFencesModule::new);
+        addModule("mcwbridges", () -> MacawBridgesModule::new);
+
+        addModule("mcwdoors", () -> MacawDoorsModule::new);
         addModule("mcwlights", () -> MacawLightsModule::new);
         addModule("mcwpaths", () -> MacawPathsModule::new);
         addModule("mcwtrpdoors", () -> MacawTrapdoorsModule::new);
-        addModule("mcwwindows", () -> MacawWindowsModule::new);
-        addModule("cfm", () -> MrCrayfishFurnitureModule::new);
-        addModule("quark", () -> QuarkModule::new);
 
 
-        ACTIVE_MODULES.forEach(m -> WoodGood.LOGGER.info("Loaded {}", m.toString()));
+        forAllModules(m -> WoodGood.LOGGER.info("Loaded {}", m.toString()));
 
         BlockSetManager.addBlockSetRegistrationCallback(this::registerWoodStuff, Block.class, WoodType.class);
         BlockSetManager.addBlockSetRegistrationCallback(this::registerLeavesStuff, Block.class, LeavesType.class);
@@ -115,6 +126,9 @@ public class WoodGood {
         bus.register(this);
 
         MinecraftForge.EVENT_BUS.addListener(CustomRecipeLoader::onEarlyPackLoad);
+        MinecraftForge.EVENT_BUS.addGenericListener(Block.class, WoodGood::remapBlocks);
+        MinecraftForge.EVENT_BUS.addGenericListener(Item.class, WoodGood::remapItems);
+        // MinecraftForge.EVENT_BUS.addGenericListener(WoodGood::remapBlocks);
 
         SERVER_RESOURCES = new ServerDynamicResourcesHandler();
         SERVER_RESOURCES.register(bus);
@@ -128,19 +142,18 @@ public class WoodGood {
 
     }
 
-
-    private void addCompetitorMod(String modId, List<String> supportedMods) {
-        if (ModList.get().isLoaded(modId)) COMPETITOR_MODS.add(new CompatMod(modId, supportedMods));
+    private void addCompetitorMod(String modId, String woodFrom, List<String> blocksFrom) {
+        COMPAT_MODS.add(new CompatMod(modId, woodFrom, blocksFrom));
     }
 
     private void addModule(String modId, Supplier<Function<String, CompatModule>> moduleFactory) {
-        if (ModList.get().isLoaded(modId)) ACTIVE_MODULES.add(moduleFactory.get().apply(modId));
+        if (ModList.get().isLoaded(modId)) ACTIVE_MODULES.put(modId, moduleFactory.get().apply(modId));
     }
 
     @SubscribeEvent
     public void init(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            ACTIVE_MODULES.forEach(CompatModule::onModSetup);
+            forAllModules(CompatModule::onModSetup);
         });
     }
 
@@ -149,32 +162,77 @@ public class WoodGood {
 
         LOGGER.info("Registering Compat Wood Blocks");
         var reg = event.getRegistry();
-        ACTIVE_MODULES.forEach(m -> m.registerWoodBlocks(reg, woods));
+        forAllModules(m -> m.registerWoodBlocks(reg, woods));
     }
 
     public void registerLeavesStuff(RegistryEvent.Register<Block> event, Collection<LeavesType> leaves) {
         LOGGER.info("Registering Compat Leaves Blocks");
         var reg = event.getRegistry();
-        ACTIVE_MODULES.forEach(m -> m.registerLeavesBlocks(reg, leaves));
+        forAllModules(m -> m.registerLeavesBlocks(reg, leaves));
     }
 
     @SubscribeEvent
     public void registerItems(RegistryEvent.Register<Item> event) {
-        ACTIVE_MODULES.forEach(m -> m.registerItems(event.getRegistry()));
+        forAllModules(m -> m.registerItems(event.getRegistry()));
     }
 
     @SubscribeEvent
     public void registerTiles(RegistryEvent.Register<BlockEntityType<?>> event) {
-        ACTIVE_MODULES.forEach(m -> m.registerTiles(event.getRegistry()));
+        forAllModules(m -> m.registerTiles(event.getRegistry()));
     }
 
     @SubscribeEvent
     public void registerEntities(RegistryEvent.Register<EntityType<?>> event) {
-        ACTIVE_MODULES.forEach(m -> m.registerEntities(event.getRegistry()));
+        forAllModules(m -> m.registerEntities(event.getRegistry()));
     }
 
-    public record CompatMod(String modId, List<String> supportedMods) {
+    public record CompatMod(String modId, String woodFrom, List<String> blocksFrom) {
 
+    }
+
+    //this can be slow. doesn't matter since it only happens once on boot
+    public static void remapBlocks(RegistryEvent.MissingMappings<Block> event) {
+        remapEntries(event, ForgeRegistries.BLOCKS);
+    }
+
+    //this can be slow. doesn't matter since it only happens once on boot
+    public static void remapItems(RegistryEvent.MissingMappings<Item> event) {
+        remapEntries(event, ForgeRegistries.ITEMS);
+    }
+
+    private static <T extends IForgeRegistryEntry<T>> void remapEntries(RegistryEvent.MissingMappings<T> event, IForgeRegistry<T> blockReg) {
+        if (!EarlyConfigs.REMAP_COMPAT.get()) return;
+        for (var compatMod : COMPAT_MODS) {
+            String woodFrom = compatMod.woodFrom; //ie bop
+            label:
+            for (var mapping : event.getMappings(compatMod.modId)) {
+                //ie: bopcomp:willow_table
+
+                for (String blockFrom : compatMod.blocksFrom) {
+                    CompatModule module = ACTIVE_MODULES.get(blockFrom); //if we have target mod
+                    // only works for simple modules
+                    if (module instanceof SimpleModule simpleModule) {
+                        //only works on simple entries
+                        for (var entry : simpleModule.getEntries()) {
+                            if (entry instanceof SimpleEntrySet se) {
+                                //find wood types from that mod id
+
+                                String s = se.getEquivalentBlock(module, mapping.key.getPath(), woodFrom);
+                                if (s != null) {
+                                    if (blockReg.containsKey(WoodGood.res(s))) {
+                                        var b = blockReg.getValue(WoodGood.res(s));
+                                        mapping.remap(b);
+                                        WoodGood.LOGGER.info("Remapping block '{}' to '{}'", mapping.key, b);
+                                        continue label;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
