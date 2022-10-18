36d90a9f6d50268c83834b2d0c1990919c28d749
MehVahdJukaar <matteogozzi1998@gmail.com>
2022-10-18 17:04:55 +0200

    added comments

--- common/src/main/java/net/mehvahdjukaar/every_compat/api/EveryCompatAPI.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/api/EveryCompatAPI.java
@@ -2,6 +2,10 @@ package net.mehvahdjukaar.every_compat.api;
 
 import net.mehvahdjukaar.every_compat.EveryCompat;
 
+/**
+ * Use this to register new wood type blocks and module
+ * To register wood types that aren't detected reference net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
+ */
 public class EveryCompatAPI {
 
     /**
@@ -17,10 +21,9 @@ public class EveryCompatAPI {
     //for each entry that you register you will need to add "block_type.everycomp.your_type" translation string to your lang file
 
     //example using simple module class
-
     /*
-        SimpleModule mod = new SimpleModule("twigs", "tw");
 
+        SimpleModule mod = new SimpleModule("twigs", "tw");
         SimpleEntrySet<?, ?> e = SimpleEntrySet.builder(WoodType.class,"table", TwigsBlocks.OAK_TABLE, ()->WoodTypeRegistry.OAK_TYPE,
                         w -> new TableBlock(Utils.copyPropertySafe(w.planks).instabreak()))
                 .addTag(new ResourceLocation("twigs:tables"), Registry.BLOCK_REGISTRY)
@@ -30,10 +33,20 @@ public class EveryCompatAPI {
                 .addTexture(new ResourceLocation("twigs:block/oak_table_top"))
                 .addTexture(new ResourceLocation("twigs:block/oak_table_bottom"))
                 .build();
-
         mod.addEntry(e);
 
         WoodGoodAPI.registerModule(mod)
+
+     */
+
+
+    //custom register a wood type
+
+    /*
+    public static void init() {
+        BlockSetAPI.addBlockTypeFinder(WoodType.class, WoodType.Finder
+            .simple("my_mod", "cherry", "cherry_plank", "cherry_stem"));
+    }
     */
 
 
