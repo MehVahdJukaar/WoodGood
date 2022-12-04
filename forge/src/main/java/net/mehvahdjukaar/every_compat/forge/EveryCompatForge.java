3c4eed07058747166cba6fdb79d2cf9a263e82e1
WenXin2 <dwnewville@gmail.com>
2022-12-04 12:50:30 -0600

    Add MosaicCarpentry Module

--- forge/src/main/java/net/mehvahdjukaar/every_compat/forge/EveryCompatForge.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/forge/EveryCompatForge.java
@@ -12,6 +12,7 @@ import net.mehvahdjukaar.every_compat.modules.farmersdelight.FarmersDelightModul
 import net.mehvahdjukaar.every_compat.modules.furnish.FurnishModule;
 import net.mehvahdjukaar.every_compat.modules.mcaw.*;
 import net.mehvahdjukaar.every_compat.modules.missing_wilds.MissingWildModule;
+import net.mehvahdjukaar.every_compat.modules.mosaic_carpentry.MosaicCarpentryModule;
 import net.mehvahdjukaar.every_compat.modules.mrcrayfish_furniture.MrCrayfishFurnitureModule;
 import net.mehvahdjukaar.every_compat.modules.pokecube.PokecubeLegendsModule;
 import net.mehvahdjukaar.every_compat.modules.quark.QuarkModule;
@@ -58,7 +59,6 @@ public class EveryCompatForge extends EveryCompat {
         addModule("mcwroofs", () -> MacawRoofsModule::new);
         addModule("mcwtrpdoors", () -> MacawTrapdoorsModule::new);
         addModule("mcwwindows", () -> MacawWindowsModule::new);
-
         addModule("mcwfences", () -> MacawFencesModule::new);
         addModule("mcwfurnitures", () -> MacawFurnitureModule::new);
         addModule("mcwbridges", () -> MacawBridgesModule::new);
@@ -75,6 +75,7 @@ public class EveryCompatForge extends EveryCompat {
         addModule("farmersdelight", () -> FarmersDelightModule::new);
         addModule("furnish", () -> FurnishModule::new);
         addModule("missingwilds", () -> MissingWildModule::new);
+        addModule("mosaic_carpentry", () -> MosaicCarpentryModule::new);
         addModule("pokecube_legends", () -> PokecubeLegendsModule::new);
         addModule("quark", () -> QuarkModule::new);
         addModule("storagedrawers", () -> StorageDrawersModule::new);
