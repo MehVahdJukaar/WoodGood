71c50457706a66619fdcc6a75919bb1dcf2646d5
WenXin2 <dwnewville@gmail.com>
2022-12-29 11:57:27 -0600

    Add tall beach door

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/dramaticdoors/DramaticDoorsMacawModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/dramaticdoors/DramaticDoorsMacawModule.java
@@ -18,6 +18,7 @@ public class DramaticDoorsMacawModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> TALL_BARN_DOORS;
     public final SimpleEntrySet<WoodType, Block> TALL_BARN_GLASSED_DOORS;
+    public final SimpleEntrySet<WoodType, Block> TALL_BEACH_DOORS;
 
     public DramaticDoorsMacawModule(String modId) {
         super(modId, "ddm");
@@ -59,5 +60,24 @@ public class DramaticDoorsMacawModule extends SimpleModule {
                 .build();
 
         this.addEntry(TALL_BARN_GLASSED_DOORS);
+
+        TALL_BEACH_DOORS = SimpleEntrySet.builder(WoodType.class, "beach_door", "tall_macaw",
+                        DDBlocks.TALL_MACAW_OAK_BEACH_DOOR, () -> WoodTypeRegistry.OAK_TYPE,
+                        w -> new TallDoorBlock(DDBlocks.getBlockByKey(new ResourceLocation("mcwdoors", "oak_beach_door"))))
+                .addTextureM(modRes("block/macaw/tall_oak_beach_door_lower"), EveryCompat.res("block/ddm/tall_oak_beach_door_lower_m"))
+                .addTextureM(modRes("block/macaw/tall_oak_beach_door_middle"), EveryCompat.res("block/ddm/tall_oak_beach_door_middle_m"))
+                .addTextureM(modRes("block/macaw/tall_oak_beach_door_upper"), EveryCompat.res("block/ddm/tall_oak_beach_door_upper_m"))
+                .addTextureM(modRes("item/macaw/tall_oak_beach_door"), EveryCompat.res("item/ddm/tall_oak_beach_door_m"))
+                .addModelTransform(m -> m.replaceGenericType("oak", "item/macaw"))
+                .addTag(modRes("tall_wooden_doors"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("tall_wooden_doors"), Registry.ITEM_REGISTRY)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
+                .setRenderType(() -> RenderType::cutout)
+                .setTab(() -> DramaticDoors.MAIN_TAB)
+                .useLootFromBase()
+                .defaultRecipe()
+                .build();
+
+        this.addEntry(TALL_BEACH_DOORS);
     }
 }
