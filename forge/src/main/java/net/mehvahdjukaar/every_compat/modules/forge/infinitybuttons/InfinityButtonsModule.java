67b7943336b42e7664701d2106cd5614071062c9
curryducker <jur.hoekman@gmail.com>
2023-08-22 15:24:30 +0200

    fix infinity buttons

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/infinitybuttons/InfinityButtonsModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/infinitybuttons/InfinityButtonsModule.java
@@ -1,7 +1,7 @@
 package net.mehvahdjukaar.every_compat.modules.forge.infinitybuttons;
 
 import net.larsmans.infinitybuttons.block.InfinityButtonsBlocks;
-import net.larsmans.infinitybuttons.block.custom.largebutton.WoodenLargeButton;
+import net.larsmans.infinitybuttons.block.custom.button.WoodenButton;
 import net.larsmans.infinitybuttons.block.custom.secretbutton.PlankSecretButton;
 import net.larsmans.infinitybuttons.item.InfinityButtonsItemGroup;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
@@ -25,7 +25,7 @@ public class InfinityButtonsModule extends SimpleModule {
 
         largeButtons = SimpleEntrySet.builder(WoodType.class, "large_button",
                         InfinityButtonsBlocks.OAK_LARGE_BUTTON, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new WoodenLargeButton(BlockBehaviour.Properties.of(Material.DECORATION).strength(0.5f).noCollission().sound(SoundType.WOOD)))
+                        w -> new WoodenButton(BlockBehaviour.Properties.of(Material.DECORATION).strength(0.5f).noCollission().sound(SoundType.WOOD), true))
                 .addTag(modRes("wooden_large_buttons"), Registry.BLOCK_REGISTRY)
                 .addTag(modRes("wooden_large_buttons"), Registry.ITEM_REGISTRY)
                 .setTab(() -> InfinityButtonsItemGroup.INFINITYBUTTONS)
@@ -36,7 +36,7 @@ public class InfinityButtonsModule extends SimpleModule {
 
         plankSecretButtons = SimpleEntrySet.builder(WoodType.class, "plank_secret_button",
                         InfinityButtonsBlocks.OAK_PLANK_SECRET_BUTTON, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new PlankSecretButton(Utils.copyPropertySafe(w.planks).strength(2.0f, 3.0f).noOcclusion().sound(SoundType.WOOD)))
+                        w -> new PlankSecretButton(Utils.copyPropertySafe(w.planks).strength(2.0f, 3.0f).noOcclusion().sound(SoundType.WOOD), w.planks))
                 .addTag(modRes("wooden_secret_buttons"), Registry.BLOCK_REGISTRY)
                 .addTag(modRes("wooden_secret_buttons"), Registry.ITEM_REGISTRY)
                 .setTab(() -> InfinityButtonsItemGroup.INFINITYBUTTONS)
