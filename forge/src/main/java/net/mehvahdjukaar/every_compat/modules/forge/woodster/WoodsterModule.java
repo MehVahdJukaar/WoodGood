ebde66e44ebeb722a23bc04267e2dba64b42fe9b
Salju <61861078+Jusey1@users.noreply.github.com>
2023-10-09 16:09:26 -0400

    Update WoodsterModule.java
    Fixed Woodster Module for 1.20 branch

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/woodster/WoodsterModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/woodster/WoodsterModule.java
@@ -27,21 +27,15 @@ public class WoodsterModule extends SimpleModule {
     public WoodsterModule(String modId) {
         super(modId, "wdst");
 
-        Supplier<WoodType> spruce = ()-> WoodTypeRegistry.getValue(new ResourceLocation("spruce"));
         chiseled_books = SimpleEntrySet.builder(WoodType.class, "chiseled_bookshelf",
-                        WoodsterBlocks.SPRUCE_CHISELED_BOOKSHELF, spruce,
+                        WoodsterBlocks.DARK_OAK_CHISELED_BOOKSHELF, () -> WoodTypeRegistry.OAK_TYPE,
                         w -> new ChiseledBookshelfBlock(Utils.copyPropertySafe(w.planks)))
-                .addTextureM(modRes("block/oak_chiseled_bookshelf_1"),modRes( "block/everycomp_chiseled_bookshelf_1"))
-                .addTextureM(modRes("block/oak_chiseled_bookshelf_2"),modRes( "block/everycomp_chiseled_bookshelf_2"))
-                .addTextureM(modRes("block/oak_chiseled_bookshelf_3"),modRes( "block/everycomp_chiseled_bookshelf_3"))
-                .addTextureM(modRes("block/oak_chiseled_bookshelf_4"),modRes( "block/everycomp_chiseled_bookshelf_4"))
-                .addTextureM(modRes("block/oak_chiseled_bookshelf_5"),modRes( "block/everycomp_chiseled_bookshelf_5"))
                 .addTextureM(modRes("block/oak_chiseled_bookshelf_6"),modRes( "block/everycomp_chiseled_bookshelf_6"))
                 .addTexture(modRes("block/oak_chiseled_bookshelf_side"))
                 .addTexture(modRes("block/oak_chiseled_bookshelf_top"))
                 .addTexture(modRes("block/oak_chiseled_bookshelf_0"))
-                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .setTab( WoodsterTabs.WOODSTER)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
+                .setTab(() -> WoodsterTabs.WOODSTER)
                 .copyParentDrop()
                 .build();
 
