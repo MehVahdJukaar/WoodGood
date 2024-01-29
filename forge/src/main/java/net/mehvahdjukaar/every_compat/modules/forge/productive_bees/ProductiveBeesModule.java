567db23da75b499f7cd79646afde3c958f523231
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-01-29 14:25:34 -0700

    Updated ProductiveBeesModule

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/productive_bees/ProductiveBeesModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/productive_bees/ProductiveBeesModule.java
@@ -8,6 +8,7 @@ import cy.jdkdigital.productivebees.init.ModItemGroups;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.moonlight.api.resources.textures.Palette;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
@@ -16,6 +17,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
 
 public class ProductiveBeesModule extends SimpleModule {
@@ -27,32 +29,41 @@ public class ProductiveBeesModule extends SimpleModule {
         super(modId, "pb");
         CreativeModeTab tab = ModItemGroups.PRODUCTIVE_BEES;
 
-
         advancedBeehives = SimpleEntrySet.builder(WoodType.class, "beehive", "advanced",
                         ModBlocks.ADVANCED_SPRUCE_BEEHIVE, () -> WoodTypeRegistry.getValue(new ResourceLocation("spruce")),
                         w -> new AdvancedBeehive(Utils.copyPropertySafe(w.planks)))
                 // Textures are not generated for this, temporary until model overrides work
                 .addModelTransform(m -> m.replaceString("buzzier_bees:block/spruce", "productivebees:block/spruce"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right_honey"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_right_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left_honey"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_left_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_right_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_left_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_right"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_side_right_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_left"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_side_left_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_top_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_top_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_top_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_top_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_m"))
-                .addTextureM(EveryCompat.res("block/spruce_beehive_front_honey"), EveryCompat.res("block/spruce_beehive_front_honey_m"))
-                .addTextureM(EveryCompat.res("block/spruce_beehive_front"), EveryCompat.res("block/spruce_beehive_front_m"))
+                // FRONT TOP
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_front_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_honey"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_front_honey_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_top_honey"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_front_top_honey_m"))
+                // FRONT SIDE
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_front_left_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_front_right_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left_honey"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_front_left_honey_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right_honey"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_front_right_honey_m"))
+                // SIDE
                 .addTextureM(EveryCompat.res("block/spruce_beehive_side"), EveryCompat.res("block/spruce_beehive_side_m"))
-                .addTexture(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_end_right"))
-                .addTexture(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_end_left"))
-                .addTexture(EveryCompat.res("block/spruce_beehive_end"))
-                .addTag(modRes("advanced_beehives"), Registry.BLOCK_REGISTRY)
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_left"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_side_left_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_right"), EveryCompat.res("block/advanced_beehive/mask/horizontal/spruce_beehive_side_right_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_side_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_side_top_m"))
+                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top_honey"), EveryCompat.res("block/advanced_beehive/mask/spruce_beehive_side_top_honey_m"))
+                // FRONT
+                .addTextureM(EveryCompat.res("block/spruce_beehive_front"), EveryCompat.res("block/spruce_beehive_front_m"))
+                .addTextureM(EveryCompat.res("block/spruce_beehive_front_honey"), EveryCompat.res("block/spruce_beehive_front_honey_m"))
+
                 .addTag(modRes("advanced_beehives"), Registry.ITEM_REGISTRY)
+                .addTag(modRes("nests/slimy_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/sugarbag_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/soul_sands_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/nether_quartz_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/end_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/nether_brick_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/nether_gold_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("nests/glowstone_nests"), Registry.BLOCK_REGISTRY)
+                .addTag(modRes("beehives"), Registry.BLOCK_REGISTRY)
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
                 .addTag(BlockTags.BEEHIVES, Registry.BLOCK_REGISTRY)
                 .addTile(ModBlockEntityTypes.ADVANCED_BEEHIVE)
@@ -61,7 +72,6 @@ public class ProductiveBeesModule extends SimpleModule {
                 .addRecipe(modRes("hives/advanced_spruce_beehive"))
                 .setTab(() -> tab)
                 .build();
-
         this.addEntry(advancedBeehives);
 
         expansionBoxes = SimpleEntrySet.builder(WoodType.class, "", "expansion_box",
@@ -69,24 +79,13 @@ public class ProductiveBeesModule extends SimpleModule {
                         w -> new ExpansionBox(Utils.copyPropertySafe(w.planks)))
                 // Textures are not generated for this, temporary until model overrides work
                 .addModelTransform(m -> m.replaceString("buzzier_bees:block/spruce", "productivebees:block/spruce"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right_honey"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_right_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left_honey"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_left_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_right"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_right_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_front_left"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_front_left_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_right"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_side_right_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_side_left"), EveryCompat.res("block/pb/advanced_beehive/horizontal/oak_beehive_side_left_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_top_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_top_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_top_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front_honey"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_honey_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side_top"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_top_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_front"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_front_m"))
-                .addTextureM(EveryCompat.res("block/advanced_beehive/spruce_beehive_side"), EveryCompat.res("block/pb/advanced_beehive/oak_beehive_side_m"))
-                .addTextureM(EveryCompat.res("block/spruce_beehive_front_honey"), EveryCompat.res("block/spruce_beehive_front_honey_m"))
+                // END -OK
+                .addTexture(EveryCompat.res("block/spruce_beehive_end"))
                 .addTexture(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_end_right"))
                 .addTexture(EveryCompat.res("block/advanced_beehive/horizontal/spruce_beehive_end_left"))
-                .addTexture(EveryCompat.res("block/spruce_beehive_front"))
-                .addTexture(EveryCompat.res("block/spruce_beehive_side"))
-                .addTexture(EveryCompat.res("block/spruce_beehive_end"))
+
+                // Using the same generated texture from Advanced Beehives'
+                .createPaletteFromOak(this::removeDarkest)
                 .addTag(modRes("expansion_boxes"), Registry.BLOCK_REGISTRY)
                 .addTag(modRes("expansion_boxes"), Registry.ITEM_REGISTRY)
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
@@ -95,8 +94,13 @@ public class ProductiveBeesModule extends SimpleModule {
                 .addRecipe(modRes("boxes/expansion_box_spruce"))
                 .setTab(() -> tab)
                 .build();
-
         this.addEntry(expansionBoxes);
 
+    }
+
+    private void removeDarkest(@NotNull Palette p) {
+        p.remove(p.getDarkest());
+        p.remove(p.getDarkest());
+        p.remove(p.getDarkest());
     }
 }
