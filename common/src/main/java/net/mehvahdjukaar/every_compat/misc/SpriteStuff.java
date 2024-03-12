59c50938766ecbe9af1357a6014d52f85b6e2b46
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-03-12 07:02:05 +0100

    sptite stuff

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/SpriteStuff.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/SpriteStuff.java
@@ -15,10 +15,11 @@ public class SpriteStuff {
 
     public static final @NotNull Predicate<String> LOOKS_LIKE_TOP_LOG_TEXTURE = (s) -> {
         s = (new ResourceLocation(s)).getPath();
+        if (s.contains("_overlay")) return false;
         return s.contains("_top") || s.contains("_end") || s.contains("_up");
     };
     public static final @NotNull Predicate<String> LOOKS_LIKE_SIDE_LOG_TEXTURE = (s) -> {
-        return !LOOKS_LIKE_TOP_LOG_TEXTURE.test(s);
+        return !LOOKS_LIKE_TOP_LOG_TEXTURE.test(s) && (new ResourceLocation(s)).getPath().contains("_overlay");
     };
     public static final @NotNull Predicate<String> LOOKS_LIKE_LEAF_TEXTURE = (s) -> {
         s = (new ResourceLocation(s)).getPath();
@@ -30,7 +31,7 @@ public class SpriteStuff {
         TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "cactus_log_top", EveryCompat.res("block/cactus_top"));
 //            TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "stripped_cactus_log", res("block/stripped_cactus_side"));
 //            TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "stripped_cactus_log_top", res("block/stripped_cactus_top"));
-        addOptional("regions_unexplored:eucalyptus", "eucalyptus_side", "regions_unexplored:block/eucalyptus_log");
+        addOptional("regions_unexplored:eucalyptus_log", "eucalyptus_side", "regions_unexplored:block/eucalyptus_log");
     }
 
     private static void addOptional(String blockId, String textureId, String texturePath) {
