3a79161f2601a5ac771f68a2e620366e358eb17a
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-03-15 19:16:31 -0600

    SpriteStuff - Corrected the texture path and added new blocks

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/SpriteStuff.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/SpriteStuff.java
@@ -19,7 +19,8 @@ public class SpriteStuff {
         return s.contains("_top") || s.contains("_end") || s.contains("_up");
     };
     public static final @NotNull Predicate<String> LOOKS_LIKE_SIDE_LOG_TEXTURE = (s) -> {
-        return !LOOKS_LIKE_TOP_LOG_TEXTURE.test(s) && !(new ResourceLocation(s)).getPath().contains("_overlay");
+        s = (new ResourceLocation(s)).getPath();
+        return (!LOOKS_LIKE_TOP_LOG_TEXTURE.test(s) && !(s.contains("_overlay") && !(s.contains("_leaves"))));
     };
     public static final @NotNull Predicate<String> LOOKS_LIKE_LEAF_TEXTURE = (s) -> {
         s = (new ResourceLocation(s)).getPath();
@@ -27,22 +28,26 @@ public class SpriteStuff {
     };
 
     public static void addHardcodedSprites() {
+        // Minecraft
         TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "cactus_log", EveryCompat.res("block/cactus_side"));
         TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "cactus_log_top", EveryCompat.res("block/cactus_top"));
 //            TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "stripped_cactus_log", res("block/stripped_cactus_side"));
 //            TextureCache.registerSpecialTextureForBlock(Blocks.CACTUS, "stripped_cactus_log_top", res("block/stripped_cactus_top"));
-        addOptional("regions_unexplored:eucalyptus_log", "eucalyptus_side", "regions_unexplored:block/eucalyptus_log");
 
-        addOptional("endlessbiomes:stripped_twisted_stem", "_top", "endlessbiomes:block/twistedstrippedlogtoptest");
+        // Endless Biomes
+        addOptional("endlessbiomes:twisted_stem", "_side", "endlessbiomes:block/twistedlogsidetest");
         addOptional("endlessbiomes:twisted_stem", "_top", "endlessbiomes:block/twistedlogtoptest");
         addOptional("endlessbiomes:stripped_twisted_stem", "_side", "endlessbiomes:block/twistedstrippedlogsidetest");
-        addOptional("endlessbiomes:twisted_stem", "_side", "endlessbiomes:block/twistedlogsidetest");
+        addOptional("endlessbiomes:stripped_twisted_stem", "_top", "endlessbiomes:block/twistedstrippedlogtoptest");
 
-        addOptional("endlessbiomes:stripped_penumbra_stem", "_top", "endlessbiomes:block/penumbrastrippedlogtoptest");
-        addOptional("endlessbiomes:penumbra_stem", "_top", "endlessbiomes:block/penumbralogtoptest");
-        addOptional("endlessbiomes:stripped_penumbra_stem", "_side", "endlessbiomes:block/penumbrastrippedlogsidetest");
-        addOptional("endlessbiomes:penumbra_stem", "_side", "endlessbiomes:block/penumbralogsidetest");
+        addOptional("endlessbiomes:penumbra_stem", "_side", "endlessbiomes:block/penumbrallogsidenewest");
+        addOptional("endlessbiomes:penumbra_stem", "_top", "endlessbiomes:block/penumbrallogtopnewest");
+        addOptional("endlessbiomes:stripped_penumbra_stem", "_side", "endlessbiomes:block/strippedpenumbralogsidenewest");
+        addOptional("endlessbiomes:stripped_penumbra_stem", "_top", "endlessbiomes:block/strippedpenumbralogtopnewest");
 
+        // Gardens Of The Dead
+        addOptional("gardens_of_the_dead:whistlecane", "_side", "gardens_of_the_dead:block/whistlecane_block");
+        addOptional("gardens_of_the_dead:whistlecane", "_top", "gardens_of_the_dead:block/whistlecane_block_top");
     }
 
     private static void addOptional(String blockId, String textureId, String texturePath) {
