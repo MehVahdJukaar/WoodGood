3621062720d159ae61d930872e3e9d25db24876b
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-04-07 22:06:04 -0600

    MacawFences (FORGE|FABRIC) - Fixed texture problems

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/mcaw/MacawFencesModule.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/mcaw/MacawFencesModule.java
@@ -2,8 +2,10 @@ package net.mehvahdjukaar.every_compat.modules.fabric.mcaw;
 
 import net.kikoz.mcwfences.init.BlockInit;
 import net.kikoz.mcwfences.util.FencesGroup;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.moonlight.api.platform.ClientPlatformHelper;
 import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesType;
 import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
@@ -16,12 +18,15 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Map;
 
 //SUPPORT v1.0.4
 public class MacawFencesModule extends SimpleModule {
@@ -61,6 +66,15 @@ public class MacawFencesModule extends SimpleModule {
                 .addTag(BlockTags.WOODEN_FENCES, Registry.BLOCK_REGISTRY)
                 .setTab(() -> FencesGroup.FENCESGROUP)
                 .defaultRecipe()
+                .addModelTransform(m -> m.addModifier((s, resLoc, w) -> {
+                    if (w.getNamespace().equals("betternether") && w.getTypeName().equals("nether_reed"))
+                        return s.replace("\"minecraft:block/stripped_oak_log\"",
+                                "\"betternether:block/nether_reed_planks_side\"");
+                    if (w.getNamespace().equals("regions_unexplored") && w.getTypeName().equals("sculkwood"))
+                        return s.replace("\"minecraft:block/stripped_oak_log\"",
+                                "\"regions_unexplored:block/sculkwood_log_dark\"");
+                    return s;
+                }))
                 .build();
         this.addEntry(HORSE_FENCES);
 
@@ -97,13 +111,41 @@ public class MacawFencesModule extends SimpleModule {
                         () -> BlockInit.OAK_HEDGE, () -> LeavesTypeRegistry.OAK_TYPE,
                         w -> {
                             var l = w.getBlockOfThis("leaves");
-                            if (l == null) return null;
+                            if (l == null || (w.getNamespace().equals("regions_unexplored")
+                                && w.getTypeName().equals("flowering"))) return null;
                             return new WallBlock(Utils.copyPropertySafe(l).lightLevel((s) -> 0));
                         })
-                .addModelTransform(m -> m.addModifier((s, id, w) ->
-                        s.replace("mcwfences:block/oak_leaves",
-                                w.getNamespace() + ":block/" + w.getTypeName() + "_leaves"))
-                )
+                .addModelTransform(m -> m.addModifier((s, id, l) -> {
+                    /*
+                     * EveryCompat's code don't account for "mcwfences:block/oak_leaves" when the mod could have used
+                     * "minecraft:block/oak_leaves" for texturing. idk why the dev use this way.
+                     */
+                    String namespace = l.getNamespace();
+                    String typeName = l.getTypeName();
+                    switch (namespace) {
+                        case "chipped" -> {
+                            return LeavesPath("", s, l, "", true);
+                        }
+                        case "betterend" -> {
+                            if (typeName.equals("lucernia")) return s.replace("\"mcwfences:block/oak_leaves\"",
+                                                        "\"betterend:block/lucernia_leaves_1\"");
+                        }
+                        case "regions_unexplored" -> {
+                            switch (typeName) {
+                                case "alpha" -> {
+                                    return LeavesPath("alpha_oak_leaves", s, l);
+                                }
+                                case "apple_oak" -> {
+                                    return LeavesPath("apple_oak_leaves_stage_0", s, l);
+                                }
+                                case "palm" -> {
+                                    return LeavesPath("palm_leaves_side", s, l);
+                                }
+                            }
+                        }
+                    }
+                    return LeavesPath("", s, l);
+                }))
                 .addTag(BlockTags.MINEABLE_WITH_HOE, Registry.BLOCK_REGISTRY)
                 .addTag(BlockTags.WALLS, Registry.BLOCK_REGISTRY)
                 .addTag(ItemTags.WALLS, Registry.ITEM_REGISTRY)
@@ -114,8 +156,77 @@ public class MacawFencesModule extends SimpleModule {
     }
 
 
-    public static class WiredFence extends FenceBlock {
+    public String LeavesPath(String leavesName, String s, LeavesType l) {
+        return LeavesPath(leavesName, s, l,  "", false, false);
+    }
+    public String LeavesPath(String leavesName, String append, String s, LeavesType l) {
+        return LeavesPath(leavesName, s, l,  "", false, false);
+    }
+    public String LeavesPath(String leavesName, String s, LeavesType l, String folderName, boolean useBLOCKS) {
+        return LeavesPath(leavesName, s, l,  folderName, useBLOCKS, false);
+    }
 
+    public String LeavesPath(String leavesName, String s, LeavesType l, String folderName, boolean useBLOCKS, boolean has_CHIPPED) {
+        String path =  "\"" + l.getNamespace();
+        path += (useBLOCKS) ? ":blocks/" : ":block/";
+        String TypeName = l.getTypeName();
+        String folder;
+        if (!leavesName.isEmpty()) {
+            if (!folderName.isEmpty()) path += folderName + "/";
+
+            return s.replace("\"mcwfences:block/oak_leaves\"",
+                    path + leavesName + "\"");
+        }
+        else if (!folderName.isEmpty()) { // only for blue_skies
+            folder = folderName + "/";
+        }
+        else if (has_CHIPPED) { // only for chipped
+            folder = TypeName.replaceAll("cherry_|frosted_|dead_|golden_|apple_|magenta_|flower_|red_|white_",
+                    "") + "/";
+        }
+        else folder = "";
+
+        return s.replace("\"mcwfences:block/oak_leaves\"",
+                path + folder + l.getTypeName() + "_leaves" + "\"");
+    }
+
+    @Override
+    public void registerBlockColors(ClientPlatformHelper.BlockColorEvent event) {
+        super.registerBlockColors(event);
+        for (Map.Entry<LeavesType, Block> entry : HEDGES.blocks.entrySet()) {
+            LeavesType t = entry.getKey();
+            Block b = entry.getValue();
+            String namespace = t.getNamespace();
+            String typeName = t.getTypeName();
+            if (namespace.equals("regions_unexplored")) {
+                if (typeName.equals("flowering")) continue;
+            }
+            else if (namespace.equals("blue_skies")) {
+                if (typeName.equals("cherry")) continue;
+            }
+            event.register((s, l, p, i) -> event.getColor(t.leaves.defaultBlockState(), l, p, i), b);
+        }
+    }
+
+    @Override
+    public void registerItemColors(ClientPlatformHelper.ItemColorEvent event) {
+        super.registerItemColors(event);
+        for (Map.Entry<LeavesType, Block> entry : HEDGES.blocks.entrySet()) {
+            LeavesType t = entry.getKey();
+            Block b = entry.getValue();
+            String namespace = t.getNamespace();
+            String typeName = t.getTypeName();
+            if (namespace.equals("regions_unexplored")) {
+                if (typeName.equals("flowering")) continue;
+            }
+            else if (namespace.equals("blue_skies")) {
+                if (typeName.equals("cherry")) continue;
+            }
+            event.register((stack, tintIndex) -> event.getColor(new ItemStack(t.leaves), tintIndex), b.asItem());
+        }
+    }
+
+    public static class WiredFence extends FenceBlock {
 
         public WiredFence(Properties properties) {
             super(properties);
--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/mcaw/MacawFencesModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/mcaw/MacawFencesModule.java
@@ -23,7 +23,9 @@ import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.WallBlock;
 
+import java.util.Map;
 
+//SUPPORT: v1.1.1+
 public class MacawFencesModule extends SimpleModule {
 
     public final SimpleEntrySet<LeavesType, Block> HEDGES;
@@ -107,10 +109,68 @@ public class MacawFencesModule extends SimpleModule {
                             if (l == null) return null;
                             return new WallBlock(Utils.copyPropertySafe(l).lightLevel((s) -> 0));
                         })
-                .addModelTransform(m -> m.addModifier((s, id, w) ->
-                        s.replace("mcwfences:block/oak_leaves",
-                                w.getNamespace() + ":block/" + w.getTypeName() + "_leaves"))
-                )
+                .addModelTransform(m -> m.addModifier((s, id, l) -> {
+                    /*
+                     * EveryCompat's code don't account for "mcwfences:block/oak_leaves" when the mod could have used
+                     * "minecraft:block/oak_leaves" for texturing. idk why the dev use this way.
+                     * FABRIC use "minecraft:block/oak_leaves" which is fine
+                     */
+                    String namespace = l.getNamespace();
+                    String typeName = l.getTypeName();
+                    switch (namespace) {
+                        case "chipped" -> {
+                            return LeavesPath("", s, l, "", true);
+                        }
+                        case "blue_skies" -> { // X
+                            if (typeName.equals("comet"))
+                                return LeavesPath("comet_leaves_grown", s, l, "leaves");
+                            if (typeName.equals("cherry"))
+                                return LeavesPath("cherry_leaves_grown", s, l, "leaves");
+
+                            return LeavesPath("", s, l, "leaves");
+                        }
+                        case "regions_unexplored" -> {
+                            switch (typeName) {
+                                case "alpha" -> {
+                                    return LeavesPath("alpha_oak_leaves", s, l);
+                                }
+                                case "apple_oak" -> {
+                                    return LeavesPath("apple_oak_leaves_stage_0", s, l);
+                                }
+                                case "small_oak" -> {
+                                    return s.replace("\"mcwfences:block/oak_leaves\"",
+                                            "\"minecraft:block/oak_leaves\"");
+                                }
+                                case "flowering" -> {
+                                    return s.replace("\"mcwfences:block/oak_leaves\"",
+                                            "\"regions_unexplored:item/flowering_leaves\"");
+                                }
+                                case "palm" -> {
+                                    return LeavesPath("palm_leaves_side", s, l);
+                                }
+                                case "enchanted_birch" -> {
+                                    return s.replace("\"mcwfences:block/oak_leaves\"",
+                                            "\"regions_unexplored:item/enchanted_birch_leaves\"");
+                                }
+                                case "silver_birch" -> {
+                                    return s.replace("\"mcwfences:block/oak_leaves\"",
+                                            "\"regions_unexplored:item/silver_birch_leaves\"");
+                                }
+                            }
+                        }
+                        case "endlessbiomes" -> { // X
+                            switch (typeName) {
+                                case "glowing_penumbral" -> {
+                                    return LeavesPath("penumbralleavesglowing", s, l,"", true);
+                                }
+                                case "penumbral" -> {
+                                    return LeavesPath("penumbralleavesnewest", s, l,"", true);
+                                }
+                            }
+                        }
+                    }
+                    return LeavesPath("", s, l);
+                }))
                 .addTag(BlockTags.MINEABLE_WITH_HOE, Registry.BLOCK_REGISTRY)
                 .addTag(BlockTags.WALLS, Registry.BLOCK_REGISTRY)
                 .addTag(ItemTags.WALLS, Registry.ITEM_REGISTRY)
@@ -120,19 +180,74 @@ public class MacawFencesModule extends SimpleModule {
         this.addEntry(HEDGES);
     }
 
+    public String LeavesPath(String leavesName, String s, LeavesType l) {
+        return LeavesPath(leavesName, s, l,  "", false, false);
+    }
+    public String LeavesPath(String leavesName, String s, LeavesType l, String folderName) {
+        return LeavesPath(leavesName, s, l,  folderName, false, false);
+    }
+    public String LeavesPath(String leavesName, String s, LeavesType l, String folderName, boolean useBLOCKS) {
+        return LeavesPath(leavesName, s, l,  folderName, useBLOCKS, false);
+    }
+
+    public String LeavesPath(String leavesName, String s, LeavesType l, String folderName, boolean useBLOCKS, boolean has_CHIPPED) {
+        String path =  "\"" + l.getNamespace();
+        path += (useBLOCKS) ? ":blocks/" : ":block/";
+        String TypeName = l.getTypeName();
+        String folder;
+        if (!leavesName.isEmpty()) {
+            if (!folderName.isEmpty()) path += folderName + "/";
+
+            return s.replace("\"mcwfences:block/oak_leaves\"",
+                    path + leavesName + "\"");
+        }
+        else if (!folderName.isEmpty()) { // only for blue_skies
+            folder = folderName + "/";
+        }
+        else if (has_CHIPPED) { // only for chipped
+            folder = TypeName.replaceAll("cherry_|frosted_|dead_|golden_|apple_|magenta_|flower_|red_|white_",
+                    "") + "/";
+        }
+        else folder = "";
+
+        return s.replace("\"mcwfences:block/oak_leaves\"",
+                path + folder + l.getTypeName() + "_leaves"+ "\"");
+    }
+
+
     @Override
     public void registerBlockColors(ClientPlatformHelper.BlockColorEvent event) {
         super.registerBlockColors(event);
-        HEDGES.blocks.forEach((t, b) -> {
+        for (Map.Entry<LeavesType, Block> entry : HEDGES.blocks.entrySet()) {
+            LeavesType t = entry.getKey();
+            Block b = entry.getValue();
+            String namespace = t.getNamespace();
+            String typeName = t.getTypeName();
+            if (namespace.equals("regions_unexplored")) {
+                if (typeName.equals("flowering")) continue;
+            }
+            else if (namespace.equals("blue_skies")) {
+                if (typeName.equals("cherry")) continue;
+            }
             event.register((s, l, p, i) -> event.getColor(t.leaves.defaultBlockState(), l, p, i), b);
-        });
+        }
     }
 
     @Override
     public void registerItemColors(ClientPlatformHelper.ItemColorEvent event) {
         super.registerItemColors(event);
-        HEDGES.blocks.forEach((t, b) -> {
+        for (Map.Entry<LeavesType, Block> entry : HEDGES.blocks.entrySet()) {
+            LeavesType t = entry.getKey();
+            Block b = entry.getValue();
+            String namespace = t.getNamespace();
+            String typeName = t.getTypeName();
+            if (namespace.equals("regions_unexplored")) {
+                if (typeName.equals("flowering")) continue;
+            }
+            else if (namespace.equals("blue_skies")) {
+                if (typeName.equals("cherry")) continue;
+            }
             event.register((stack, tintIndex) -> event.getColor(new ItemStack(t.leaves), tintIndex), b.asItem());
-        });
+        }
     }
 }
