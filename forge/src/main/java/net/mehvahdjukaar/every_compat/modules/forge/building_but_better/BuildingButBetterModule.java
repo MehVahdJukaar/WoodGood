0bbdbf8e5864d4a41d67257b66eaa8ef984dd433
Mallory Hayr <hey@igalaxy.dev>
2024-08-09 00:26:34 -0700

    Remove unused imports

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/building_but_better/BuildingButBetterModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/building_but_better/BuildingButBetterModule.java
@@ -12,9 +12,7 @@ import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class BuildingButBetterModule extends SimpleModule {
