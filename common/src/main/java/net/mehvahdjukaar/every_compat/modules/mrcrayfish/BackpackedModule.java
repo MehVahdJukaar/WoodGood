7ce17565b2ed335fbaeda7127633070d583a7370
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-09-03 19:14:04 -0600

    BackpackedModule: Moved to COMMON folder

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/mrcrayfish/BackpackedModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/mrcrayfish/BackpackedModule.java
@@ -1,11 +1,14 @@
-package net.mehvahdjukaar.every_compat.modules.forge.backpacked;
+package net.mehvahdjukaar.every_compat.modules.mrcrayfish;
 
 import com.mrcrayfish.backpacked.block.ShelfBlock;
+import com.mrcrayfish.backpacked.blockentity.ShelfBlockEntity;
+import com.mrcrayfish.backpacked.client.renderer.entity.layers.ShelfRenderer;
 import com.mrcrayfish.backpacked.core.ModBlockEntities;
 import com.mrcrayfish.backpacked.core.ModBlocks;
 import com.mrcrayfish.backpacked.core.ModCreativeTabs;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
@@ -13,8 +16,9 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
-//credit to WenXin2
+//SUPPORT: v2.2.3+
 public class BackpackedModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> shelves;
@@ -24,7 +28,7 @@ public class BackpackedModule extends SimpleModule {
 
         shelves = SimpleEntrySet.builder(WoodType.class, "backpack_shelf",
                         ModBlocks.OAK_BACKPACK_SHELF::get, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new ShelfBlock(Utils.copyPropertySafe(w.log)))
+                        w -> new ShelfBlock(Utils.copyPropertySafe(w.planks)))
                 .requiresChildren("stripped_log")
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setTab(ModCreativeTabs.MAIN::get)
@@ -32,7 +36,6 @@ public class BackpackedModule extends SimpleModule {
                 .addTile(ModBlockEntities.SHELF::get)
                 .setRenderType(() -> RenderType::cutout)
                 .build();
-
         this.addEntry(shelves);
     }
 }
