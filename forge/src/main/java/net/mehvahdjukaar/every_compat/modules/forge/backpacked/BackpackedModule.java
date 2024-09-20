28c86a7c406376765bcfbf588a995b5cfd1c33d2
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-09-19 20:47:33 -0600

    BackpackedModule: Updated

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/backpacked/BackpackedModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/backpacked/BackpackedModule.java
@@ -2,27 +2,21 @@ package net.mehvahdjukaar.every_compat.modules.forge.backpacked;
 
 import com.mrcrayfish.backpacked.Backpacked;
 import com.mrcrayfish.backpacked.block.ShelfBlock;
-import com.mrcrayfish.backpacked.client.renderer.entity.layers.ShelfRenderer;
 import com.mrcrayfish.backpacked.core.ModBlockEntities;
 import com.mrcrayfish.backpacked.core.ModBlocks;
-import com.mrcrayfish.backpacked.tileentity.ShelfBlockEntity;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.moonlight.api.platform.ClientPlatformHelper;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
-import vectorwing.farmersdelight.common.registry.ModBlockEntityTypes;
 
 //credit to WenXin2
+
+//SUPPORT: v2.1.13-FINAL
 public class BackpackedModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> shelves;
@@ -32,14 +26,15 @@ public class BackpackedModule extends SimpleModule {
 
         shelves = SimpleEntrySet.builder(WoodType.class, "backpack_shelf",
                         ModBlocks.OAK_BACKPACK_SHELF, () -> WoodTypeRegistry.OAK_TYPE,
-                ifHasChild(w -> new ShelfBlock(Utils.copyPropertySafe(w.log)), "stripped_log"))
+                        w -> new ShelfBlock(Utils.copyPropertySafe(w.log))
+                )
+                .requiresChildren("slab") // Recipes
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registry.BLOCK_REGISTRY)
                 .setTab(() -> Backpacked.TAB)
                 .addRecipe(modRes("oak_backpack_shelf"))
                 .addTile(ModBlockEntities.SHELF)
                 .setRenderType(() -> RenderType::cutout)
                 .build();
-
         this.addEntry(shelves);
     }
 }
