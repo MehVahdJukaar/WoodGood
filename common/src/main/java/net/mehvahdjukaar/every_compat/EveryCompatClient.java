4f11fe97dcc822f02670009442b5d851adb2429d
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-09-25 15:24:41 -0600

    EveryCompatClient: Added BlockTags to Tooltips

--- common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatClient.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatClient.java
@@ -2,7 +2,6 @@ package net.mehvahdjukaar.every_compat;
 
 import net.mehvahdjukaar.every_compat.api.CompatModule;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
-import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.configs.ModConfigs;
 import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
 import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
@@ -13,10 +12,12 @@ import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -69,7 +70,16 @@ public class EveryCompatClient {
             }
         }
         if (PlatHelper.isDev()) {
-            stack.getTags().forEach((k) -> components.add(Component.literal("ItemTag: " + k.location()).withStyle(ChatFormatting.GRAY)));
+            Block blocked = Block.byItem(stack.getItem());
+            // BLOCK TAGS
+            if (blocked != Blocks.AIR) {
+                components.add(Component.literal("BlockTag:").withStyle(ChatFormatting.GREEN));
+                blocked.defaultBlockState().getTags().forEach((k) -> components.add(Component.literal("-" + k.location()).withStyle(Style.EMPTY.withColor(0xc8ffc8))));
+            }
+
+            // ITEM TAGS
+            components.add(Component.literal("ItemTag:").withStyle(ChatFormatting.LIGHT_PURPLE));
+            stack.getTags().forEach((k) -> components.add(Component.literal("-" + k.location()).withStyle(Style.EMPTY.withColor(0xffc8ff))));
         }
     }
 
