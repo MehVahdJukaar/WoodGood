c5685f6f2028f8d58ef8982051e5abafa3c44f9b
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-10-07 00:08:55 -0600

    EveryCompatClient: Added Tooltips for tags

--- common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatClient.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompatClient.java
@@ -2,8 +2,22 @@ package net.mehvahdjukaar.every_compat;
 
 import net.mehvahdjukaar.every_compat.api.CompatModule;
 import net.mehvahdjukaar.moonlight.api.platform.ClientPlatformHelper;
+import net.mehvahdjukaar.moonlight.api.platform.PlatformHelper;
+import net.mehvahdjukaar.moonlight.api.set.BlockType;
+import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesTypeRegistry;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.List;
 
 
 public class EveryCompatClient {
@@ -34,6 +48,21 @@ public class EveryCompatClient {
 
     public static void clientSetup() {
         EveryCompat.forAllModules(CompatModule::onClientSetup);
+    }
+
+    public static void onItemTooltip(ItemStack stack, TooltipFlag tooltipFlag, List<Component> components) {
+        if (PlatformHelper.isDev()) {
+            Block blocked = Block.byItem(stack.getItem());
+            // BLOCK TAGS
+            if (blocked != Blocks.AIR) {
+                components.add(Component.literal("BlockTag:").withStyle(ChatFormatting.GREEN));
+                blocked.defaultBlockState().getTags().forEach((k) -> components.add(Component.literal("-" + k.location()).withStyle(Style.EMPTY.withColor(0xc8ffc8))));
+            }
+
+            // ITEM TAGS
+            components.add(Component.literal("ItemTag:").withStyle(ChatFormatting.LIGHT_PURPLE));
+            stack.getTags().forEach((k) -> components.add(Component.literal("-" + k.location()).withStyle(Style.EMPTY.withColor(0xffc8ff))));
+        }
     }
 
 }
