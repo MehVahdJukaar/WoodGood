f99e36ee0ae43a7b487ae7b675b0e0a1304f03d1
Xelbayria <12866666+Xelbayria@users.noreply.github.com>
2024-10-12 17:58:23 -0600

    MacawLightsModule(BOTH): Fixed #642

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/mcaw/MacawLightsModule.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/mcaw/MacawLightsModule.java
@@ -8,15 +8,16 @@ import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 
-
+//SUPPORT: v1.0.6+
 public class MacawLightsModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> SOUL_TIKI_TORCHES;
@@ -28,26 +29,32 @@ public class MacawLightsModule extends SimpleModule {
 
         SOUL_TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch", "soul",
                         () -> BlockInit.SOUL_OAK_TIKI_TORCH, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new TikiTorch(Utils.copyPropertySafe(w.planks)
-                                .strength(0.2f, 2.5f), ParticleTypes.SOUL_FIRE_FLAME))
+                        w -> new TikiTorch(BlockBehaviour.Properties.of()
+                                .mapColor(MapColor.WOOD)
+                                .noOcclusion(),
+                                ParticleTypes.SOUL_FIRE_FLAME
+                        )
+                )
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setRenderType(RenderLayer.CUTOUT)
                 .defaultRecipe()
                 .setTabKey(tab)
                 .build();
-
         this.addEntry(SOUL_TIKI_TORCHES);
 
         TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch",
                         () -> BlockInit.OAK_TIKI_TORCH, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new TikiTorch(Utils.copyPropertySafe(w.planks)
-                                .strength(0.2f, 2.5f), ParticleTypes.FLAME))
+                        w -> new TikiTorch(BlockBehaviour.Properties.of()
+                                .mapColor(MapColor.WOOD)
+                                .noOcclusion(),
+                                ParticleTypes.FLAME
+                        )
+                )
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setRenderType(RenderLayer.CUTOUT)
                 .setTabKey(tab)
                 .defaultRecipe()
                 .build();
-
         this.addEntry(TIKI_TORCHES);
     }
 }
--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/mcaw/MacawLightsModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/mcaw/MacawLightsModule.java
@@ -1,20 +1,21 @@
 package net.mehvahdjukaar.every_compat.modules.forge.mcaw;
 
 import com.mcwlights.kikoz.init.BlockInit;
-import com.mcwlights.kikoz.init.TabInit;
 import com.mcwlights.kikoz.objects.TikiTorch;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 
-
+//SUPPORT: v1.0.6+
 public class MacawLightsModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> SOUL_TIKI_TORCHES;
@@ -22,28 +23,38 @@ public class MacawLightsModule extends SimpleModule {
 
     public MacawLightsModule(String modId) {
         super(modId, "mcl");
-        var tab = TabInit.LIGHTSITEMGROUP;
+        var tab = modRes(modId);
 
         SOUL_TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch", "soul",
                         BlockInit.SOUL_OAK_TIKI_TORCH, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new TikiTorch(Utils.copyPropertySafe(w.planks).strength(0.2f, 2.5f), ParticleTypes.SOUL_FIRE_FLAME))
+                        w -> new TikiTorch(BlockBehaviour.Properties.of()
+                                .mapColor(MapColor.WOOD)
+                                .strength(1.5F, 2.5F)
+                                .sound(SoundType.WOOD)
+                                .noOcclusion(), ParticleTypes.SOUL_FIRE_FLAME
+                        )
+                )
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setRenderType(RenderLayer.CUTOUT)
                 .defaultRecipe()
-                .setTab(tab)
+                .setTabKey(tab)
                 .build();
-
         this.addEntry(SOUL_TIKI_TORCHES);
 
         TIKI_TORCHES = SimpleEntrySet.builder(WoodType.class, "tiki_torch",
                         BlockInit.OAK_TIKI_TORCH, () -> WoodTypeRegistry.OAK_TYPE,
-                        w -> new TikiTorch(Utils.copyPropertySafe(w.planks).strength(0.2f, 2.5f), ParticleTypes.FLAME))
+                        w -> new TikiTorch(BlockBehaviour.Properties.of()
+                                .mapColor(MapColor.WOOD)
+                                .strength(1.5F, 2.5F)
+                                .sound(SoundType.WOOD)
+                                .noOcclusion(), ParticleTypes.FLAME
+                        )
+                )
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setRenderType(RenderLayer.CUTOUT)
                 .defaultRecipe()
-                .setTab(tab)
+                .setTabKey(tab)
                 .build();
-
         this.addEntry(TIKI_TORCHES);
     }
 }
