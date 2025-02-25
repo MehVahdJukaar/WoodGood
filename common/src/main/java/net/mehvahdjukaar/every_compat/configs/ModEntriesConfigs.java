9952c16c9976bfa6a88b46f607689f57b1b370ad
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-02-26 00:48:03 +0100

    config stuff

--- common/src/main/java/net/mehvahdjukaar/every_compat/configs/ModEntriesConfigs.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/configs/ModEntriesConfigs.java
@@ -51,7 +51,8 @@ public class ModEntriesConfigs {
         builder.comment("Disables specific entries")
                 .push("entries");
         for (var reg : BlockSetAPI.getRegistries()) {
-            if (reg.getType() == WoodType.class || reg.getType() == LeavesType.class) {
+            if (reg.getType() == WoodType.class || reg.getType() == LeavesType.class ||
+            reg.typeName().equals("mud_type") || reg.typeName().equals("stone_type")) {
                 builder.push(reg.typeName().replace(" ", "_"));
                 for (var c : EveryCompat.getChildKeys(reg.getType())) {
                     String key = c.replace(":", ".");
