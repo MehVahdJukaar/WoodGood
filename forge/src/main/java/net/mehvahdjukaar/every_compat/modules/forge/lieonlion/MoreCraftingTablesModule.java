3e70e1bc0656c57d123440b1df9a71b6c0dca6a3
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-03-01 04:05:14 -0700

    MoreCraftingTablesModule: Changed EveryCompat.res() to modRes()

--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/lieonlion/MoreCraftingTablesModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/lieonlion/MoreCraftingTablesModule.java
@@ -6,19 +6,12 @@ import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.common.Tags;
-
-import java.util.function.Supplier;
 
 // SUPPORT: v1.2.7+
 // NOTE: More Crafting Table is developed by LieOnLion and has both FORGE & FABRIC
@@ -34,9 +27,9 @@ public class MoreCraftingTablesModule extends SimpleModule {
                         getModBlock("spruce_crafting_table"),
                         () -> WoodTypeRegistry.getValue(new ResourceLocation("spruce")),
                         w -> new MoreCraftingTableBlock(MapColor.WOOD))
-                .addTextureM(EveryCompat.res("block/spruce_crafting_table_front"), EveryCompat.res("block/lolmct/spruce_crafting_table_front_m"))
-                .addTextureM(EveryCompat.res("block/spruce_crafting_table_side"), EveryCompat.res("block/lolmct/spruce_crafting_table_side_m"))
-                .addTextureM(EveryCompat.res("block/spruce_crafting_table_top"), EveryCompat.res("block/lolmct/spruce_crafting_table_top_m"))
+                .addTextureM(modRes("block/spruce_crafting_table_front"), EveryCompat.res("block/lolmct/spruce_crafting_table_front_m"))
+                .addTextureM(modRes("block/spruce_crafting_table_side"), EveryCompat.res("block/lolmct/spruce_crafting_table_side_m"))
+                .addTextureM(modRes("block/spruce_crafting_table_top"), EveryCompat.res("block/lolmct/spruce_crafting_table_top_m"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .addTag(new ResourceLocation("forge:workbench"), Registries.BLOCK)
                 .addTag(new ResourceLocation("lieonstudio:crafting_tables"), Registries.BLOCK)
