eedb951e7bf3a8a23e6f2e69cbcd908eba70ea83
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-03-01 21:41:57 -0700

    StorageDrawersModule: Removed unneeded addTexture()

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/storagedrawers/StorageDrawersModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/storagedrawers/StorageDrawersModule.java
@@ -69,9 +69,6 @@ public class StorageDrawersModule extends SimpleModule {
                 .addTile(getModTile("standard_drawers_2"))
                 .createPaletteFromPlanks(this::drawersPalette)
                 .addTexture(modRes("block/drawers_oak_front_2"))
-//                .addTexture(modRes("block/drawers_oak_side"))
-//                .addTexture(modRes("block/drawers_oak_sort"))
-//                .addTexture(modRes("block/drawers_oak_trim"))
                 .build();
         this.addEntry(FULL_DRAWERS_2);
 
@@ -88,9 +85,6 @@ public class StorageDrawersModule extends SimpleModule {
                 .addTile(getModTile("standard_drawers_4"))
                 .createPaletteFromPlanks(this::drawersPalette)
                 .addTexture(modRes("block/drawers_oak_front_4"))
-//                .addTexture(modRes("block/drawers_oak_side"))
-//                .addTexture(modRes("block/drawers_oak_sort"))
-//                .addTexture(modRes("block/drawers_oak_trim"))
                 .build();
 
         this.addEntry(FULL_DRAWERS_4);
@@ -107,12 +101,8 @@ public class StorageDrawersModule extends SimpleModule {
                 .defaultRecipe()
                 .addTile(getModTile("standard_drawers_1"))
                 .createPaletteFromPlanks(this::drawersPalette)
-//                .addTexture(modRes("block/drawers_oak_front_1"))
-//                .addTexture(modRes("block/drawers_oak_side"))
                 .addTexture(modRes("block/drawers_oak_side_h"))
                 .addTexture(modRes("block/drawers_oak_side_v"))
-//                .addTexture(modRes("block/drawers_oak_sort"))
-//                .addTexture(modRes("block/drawers_oak_trim"))
                 .build();
         this.addEntry(HALF_DRAWERS_1);
 
@@ -128,12 +118,6 @@ public class StorageDrawersModule extends SimpleModule {
                 .defaultRecipe()
                 .addTile(getModTile("standard_drawers_2"))
                 .createPaletteFromPlanks(this::drawersPalette)
-//                .addTexture(modRes("block/drawers_oak_front_2"))
-//                .addTexture(modRes("block/drawers_oak_side"))
-//                .addTexture(modRes("block/drawers_oak_side_h"))
-//                .addTexture(modRes("block/drawers_oak_side_v"))
-//                .addTexture(modRes("block/drawers_oak_sort"))
-//                .addTexture(modRes("block/drawers_oak_trim"))
                 .build();
         this.addEntry(HALF_DRAWERS_2);
 
@@ -149,12 +133,6 @@ public class StorageDrawersModule extends SimpleModule {
                 .defaultRecipe()
                 .addTile(getModTile("standard_drawers_4"))
                 .createPaletteFromPlanks(this::drawersPalette)
-//                .addTexture(modRes("block/drawers_oak_front_4"))
-//                .addTexture(modRes("block/drawers_oak_side"))
-//                .addTexture(modRes("block/drawers_oak_side_h"))
-//                .addTexture(modRes("block/drawers_oak_side_v"))
-//                .addTexture(modRes("block/drawers_oak_sort"))
-//                .addTexture(modRes("block/drawers_oak_trim"))
                 .build();
         this.addEntry(HALF_DRAWERS_4);
 
