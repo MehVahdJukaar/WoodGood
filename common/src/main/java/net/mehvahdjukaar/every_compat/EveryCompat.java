cb7acbbc23494878de26b4152ef1b392517617e0
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-03-14 16:36:14 -0600

    EveryCompat: Added a comment to reply Meh's question

--- common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompat.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompat.java
@@ -233,6 +233,7 @@ public abstract class EveryCompat {
 
     //do all childen exist for all wood types for this child type
     //TODO: you sure this is correct? we arent using this anywhere else. its fine to have stuf generate JUST for the wood types that have the chlidren. imagine adding a mod without "fences" and all your other "fences" reliant blocks from other mod suddenly dont wok anymore
+    //!! ATTENTION: Meh, it's working properly and if you look at its usage, there are 6 usages for it. Dont worry about this, i'll move it later
     public static boolean doChildrenExistFor(WoodType w, String... blockTypes) {
         for (String type : blockTypes) {
             if (w.getBlockOfThis(type) == null) return false;
