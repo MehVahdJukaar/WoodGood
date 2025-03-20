9e696976f410c680acc7ff5c552cf0c541b1f36e
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-03-20 01:37:35 -0600

    Revert "SimpleModule: Updated getEntry()"
    This reverts commit 76028a32e94ee593c4b0833b4fdfb604ac98df23.

--- common/src/main/java/net/mehvahdjukaar/every_compat/api/SimpleModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/api/SimpleModule.java
@@ -69,6 +69,9 @@ public class SimpleModule extends CompatModule {
     }
 
     public EntrySet<?> getEntry(String name) {
+//        var e = entries.get(name);
+//        if (e == null)
+//            throw new UnsupportedOperationException(String.format("This module does not have entries of type %s", name));
         return entries.get(name);
     }
 
