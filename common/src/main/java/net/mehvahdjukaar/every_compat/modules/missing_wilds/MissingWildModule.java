f461c2908ff1eb922bfbd4647592078c3400ae59
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-23 17:39:25 -0600

    build.gradle(ALL): Added Missing-Wild

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/missing_wilds/MissingWildModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/missing_wilds/MissingWildModule.java
@@ -1,18 +1,17 @@
 package net.mehvahdjukaar.every_compat.modules.missing_wilds;
 
 import me.ultrusmods.missingwilds.block.FallenLogBlock;
-import me.ultrusmods.missingwilds.register.MissingWildsBlocks;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.every_compat.misc.VanillaWoods;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
-//SUPPORT v1.2.0+
+//SUPPORT v4.0.0-BETA+
 public class MissingWildModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> fallenLogs;
@@ -21,7 +20,7 @@ public class MissingWildModule extends SimpleModule {
         super(modId, "msw");
 
         fallenLogs = SimpleEntrySet.builder(WoodType.class, "log", "fallen",
-                        MissingWildsBlocks.FALLEN_ACACIA_LOG, () -> WoodTypeRegistry.getValue(VanillaWoods.ACACIA),
+                        getModBlock("fallen_acacia_log"), () -> WoodTypeRegistry.getValue(VanillaWoods.ACACIA),
                         w -> new FallenLogBlock(Utils.copyPropertySafe(w.log).noOcclusion()))
                 .addTag(modRes("fallen_logs"), Registries.BLOCK)
                 .addTag(modRes("fallen_logs"), Registries.ITEM)
