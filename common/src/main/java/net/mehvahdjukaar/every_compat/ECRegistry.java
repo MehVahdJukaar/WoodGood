032556a72948023c2332eba482e5f8b6096200e7
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-25 03:10:28 -0600

    ECRegistry: Removed addBlocksToPOI()

--- common/src/main/java/net/mehvahdjukaar/every_compat/ECRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/ECRegistry.java
@@ -1,19 +1,11 @@
 package net.mehvahdjukaar.every_compat;
 
-import com.google.common.collect.ImmutableSet;
 import net.mehvahdjukaar.every_compat.configs.ECConfigs;
 import net.mehvahdjukaar.every_compat.misc.AllWoodItem;
 import net.mehvahdjukaar.moonlight.api.misc.RegSupplier;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.entity.ai.village.poi.PoiType;
-import net.minecraft.world.entity.ai.village.poi.PoiTypes;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
@@ -36,16 +28,4 @@ public class ECRegistry {
                                     .build())
                     : null;
 
-    //TODO: use ML one when it updates
-    @Deprecated(forRemoval = true)
-    //call in setup when you have blocks
-    public static void addBlocksToPOI(ResourceKey<PoiType> poi, Iterable<? extends Block> blocks) {
-        Holder.Reference<PoiType> holderPOI = BuiltInRegistries.POINT_OF_INTEREST_TYPE.getHolderOrThrow(poi);
-        ImmutableSet.Builder<BlockState> builder = ImmutableSet.builder();
-        builder.addAll(holderPOI.value().matchingStates());
-        for (var block : blocks) {
-            builder.addAll(block.getStateDefinition().getPossibleStates());
-        }
-        PoiTypes.registerBlockStates(holderPOI, builder.build());
-    }
 }
