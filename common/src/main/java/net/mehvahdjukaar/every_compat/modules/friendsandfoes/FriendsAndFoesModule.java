34b7d573b98499fe64e3b2245c5a06b82e00050e
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-25 03:12:43 -0600

    FriendsAndFoesModule: Cleanup unneeded imports

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/friendsandfoes/FriendsAndFoesModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/friendsandfoes/FriendsAndFoesModule.java
@@ -1,34 +1,23 @@
 package net.mehvahdjukaar.every_compat.modules.friendsandfoes;
 
-import com.google.common.collect.ImmutableSet;
-import net.mehvahdjukaar.every_compat.ECRegistry;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.every_compat.dynamicpack.ServerDynamicResourcesHandler;
 import net.mehvahdjukaar.every_compat.misc.VanillaWoods;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
-import net.mehvahdjukaar.moonlight.api.resources.SimpleTagBuilder;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.PoiTypeTags;
-import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.entity.ai.village.poi.PoiTypes;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
 
-import java.util.Set;
-import java.util.function.Supplier;
-
+//SUPPORT: v3.0.9+
 public class FriendsAndFoesModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> beehives;
