16eac7d8f1e0809bac21f2c953a5c78f06cdaa55
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-05-05 21:40:09 -0600

    RecipeUtility: Updated the method to use ResourceSink

--- common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/RecipeUtility.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/RecipeUtility.java
@@ -1,9 +1,10 @@
 package net.mehvahdjukaar.every_compat.common_classes;
 
 import com.google.gson.JsonObject;
-import net.mehvahdjukaar.every_compat.dynamicpack.ServerDynamicResourcesHandler;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
 import net.mehvahdjukaar.moonlight.api.resources.ResType;
+import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceSink;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -21,7 +22,7 @@ public class RecipeUtility {
      * Create Stonecutting Recipe that use tag as an ingredient
      */
     public static void stonecuttingWithTagRecipe(Block output, ResourceLocation recipeLoc, ResourceLocation tagResLoc,
-                                                 ResourceLocation newRecipeLoc, ServerDynamicResourcesHandler handler, ResourceManager manager) {
+                                                 ResourceLocation newRecipeLoc, ResourceSink sink, ResourceManager manager) {
         if (Objects.nonNull(output)) {
             try (InputStream recipeStream = manager.getResource(recipeLoc)
                     .orElseThrow(() -> new FileNotFoundException("Failed to get " + recipeLoc)).open()) {
@@ -32,10 +33,10 @@ public class RecipeUtility {
                 recipe.addProperty("result", Utils.getID(output).toString());
 
                 // Adding to the resources
-                handler.dynamicPack.addJson(newRecipeLoc, recipe, ResType.RECIPES);
+                sink.addJson(newRecipeLoc, recipe, ResType.RECIPES);
 
             } catch (IOException e) {
-                handler.getLogger().error("Failed to generate the recipe @ {} : {}", recipeLoc, e);
+                EveryCompat.LOGGER.error("Failed to generate the recipe @ {} : {}", recipeLoc, e);
             }
         }
     }
