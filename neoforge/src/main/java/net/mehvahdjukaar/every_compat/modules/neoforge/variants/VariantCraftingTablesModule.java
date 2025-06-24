28b4f0ede9ef9d83521fdfcbcf8aa92d46f114e8
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-06-24 17:52:08 -0600

    VariantCraftingTablesModule: Added comment, //SUPPORT

--- neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/variants/VariantCraftingTablesModule.java
+++ neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/variants/VariantCraftingTablesModule.java
@@ -17,7 +17,7 @@ import net.minecraft.world.level.block.SoundType;
 
 import java.util.List;
 
-
+//SUPPORT: NOT_AVAILABLE (Kitteh6660)
 public class VariantCraftingTablesModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> craftingTable;
