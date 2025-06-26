1e2312c8d0adbc2295778a7a3862f232986d4b41
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-06-26 01:11:22 -0600

    EveryCompat: removed an extra space

--- common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompat.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/EveryCompat.java
@@ -74,7 +74,7 @@ public abstract class EveryCompat {
         addOtherCompatMod(compatModId, List.of(fromModId), List.of(supportedModId));
     }
 
-    public static  void addOtherCompatMod(String compatModId, String fromModId, String... supportedModId) {
+    public static void addOtherCompatMod(String compatModId, String fromModId, String... supportedModId) {
         List<String> list = new ArrayList<>();
         Collections.addAll(list, supportedModId);
         addOtherCompatMod(compatModId, List.of(fromModId), list);
