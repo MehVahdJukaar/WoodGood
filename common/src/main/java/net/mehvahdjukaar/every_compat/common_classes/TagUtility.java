ebba3cde7841048fb13d5bc807be7a96bd727773
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-07-12 14:09:00 -0600

    TagUtility: Updated glass tags for FABRIC & FORGE

--- common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/TagUtility.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/TagUtility.java
@@ -185,13 +185,25 @@ public class TagUtility {
     
     // Common tags
 
-    private static ResourceLocation commonTag(String suffix) {
+    @SuppressWarnings("SameParameterValue")
+    private static ResourceLocation commonTag(String tagPath) {
         return PlatHelper.getPlatform().isFabric() ?
-                new ResourceLocation("c", suffix) :
-                new ResourceLocation("forge", suffix);
+                new ResourceLocation("c", tagPath) :
+                new ResourceLocation("forge", tagPath);
     }
 
-    public static final ResourceLocation GLASS_TAG = commonTag("glass");
+    public static ResourceLocation fabricTag(String tagPath) {
+        return new ResourceLocation("c", tagPath);
+    }
+
+    public static ResourceLocation forgeTag(String tagPath) {
+        return new ResourceLocation("forge", tagPath);
+    }
+
+    public static final ResourceLocation SILICA_TAG = PlatHelper.getPlatform().isFabric() ? fabricTag("silica_glass") : forgeTag("silica");
+    public static final ResourceLocation GLASS_TAG = PlatHelper.getPlatform().isFabric() ? fabricTag("glass_blocks") : forgeTag("glass");
     public static final ResourceLocation GLASS_PANE_TAG = commonTag("glass_panes");
+    public static final ResourceLocation GLASS_COLORLESS_TAG = PlatHelper.getPlatform().isFabric() ? fabricTag("colorless_glass") : forgeTag("glass/colorless");
+    public static final ResourceLocation GLASS_PANE_COLORLESS_TAG = PlatHelper.getPlatform().isFabric() ? fabricTag("colorless_glass_panes") : forgeTag("glass_panes/colorless");
 
 }
