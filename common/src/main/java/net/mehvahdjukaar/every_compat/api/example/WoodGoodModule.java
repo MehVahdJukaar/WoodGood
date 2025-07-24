36f1abbbc4ab0ccc20152cfadeb8110653c465bc
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-07-24 02:54:15 -0600

    WoodGoodModule: Updated the javadoc further

--- common/src/main/java/net/mehvahdjukaar/every_compat/api/example/WoodGoodModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/api/example/WoodGoodModule.java
@@ -5,17 +5,22 @@ import net.mehvahdjukaar.every_compat.api.ItemOnlyEntrySet;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
+import net.mehvahdjukaar.every_compat.common_classes.TagUtility;
 import net.mehvahdjukaar.every_compat.misc.SpriteHelper;
+import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+
+import java.util.function.Consumer;
 
 ///      ┌──────────────────────────────────────────────────────────┐
 ///      │       WoodGoodModule using Twigs Mod as An Example       │
@@ -29,6 +34,7 @@ public class WoodGoodModule extends SimpleModule {
         public final ItemOnlyEntrySet<WoodType, Item> sampleItem;
 
         public WoodGoodModule(String modId) {
+            // an example of shortened ID for TerraFirmaCraft is "tfc", so one for Twigs is "tw"
             super(modId, "tw");
 
             //  your mod's tab or minecraft's tab can be used for setTabKey() - it can use either ResourceKey or ResourceLocation
@@ -39,6 +45,7 @@ public class WoodGoodModule extends SimpleModule {
                     getModBlock("oak_table"), ()-> WoodTypeRegistry.OAK_TYPE,
                     woodType -> new TableBlock(Utils.copyPropertySafe(woodType.planks).instabreak())
                     )
+                    /// Similar setup as sampleBlock
                     .build();
             this.addEntry(sampleBlock_2);
 
@@ -46,35 +53,40 @@ public class WoodGoodModule extends SimpleModule {
                             getModBlock("oak_table"), ()-> WoodTypeRegistry.OAK_TYPE,
                             woodType -> new TableBlock(Utils.copyPropertySafe(woodType.planks).instabreak())
                     )
-                    /// Check if a WoodType or LeavesType has the children required, then block will be generated
+                    ///OPTIONAL: Check if a WoodType or LeavesType has the children required, then block will be generated
                     .requiresChildren("slab", "other_childkey") //REASON: can be for recipes or textures
                     .requiresFromMap(sampleBlock_2.blocks) // If your block required another block for crafting or texturing
                     //NOTE: sampleBlock_2 has to be above of this EntrySet for .requiresFromMap to work properly
 
-                    /// Add the block to EntityBlockType
+                    ///OPTIONAL: Add the block to EntityBlockType
                     .addTile(getModTile("id_of_EntityType"))
 
-                    /// Adding block's textures to be generated
+                    ///OPTIONAL: Adding block's textures to be generated
                     .createPaletteFromChild("log", SpriteHelper.LOOKS_LIKE_SIDE_LOG_TEXTURE) // Without this, the default texture is planks' texture. but this is using log's SIDE texture
                     .addTexture(modRes("block/oak_table"))
                     .addTexture(new ResourceLocation("twigs:block/oak_table_bottom"))
                     .addTextureM(modRes("block/oak_table_top"), modRes("block/mask/oak_table_top_m")) // If the texture has parts that shouldn't be recolored, the mask (black color) can be used to exclude them
 
-                    /// Adding tags to the block
+                    ///OPTIONAL: Adding tags to the block
+                    .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                     .addTag(new ResourceLocation("twigs:tables"), Registries.BLOCK)
                     .addTag(new ResourceLocation("twigs:tables"), Registries.ITEM)
 
-                    /// Add block to your mod's tab or Minecraft's tab
+                    ///OPTIONAL: If your block is glasses, then please take a look at below & Looking for // Common Tags which is at the bottom of the class
+            // https://github.com/MehVahdJukaar/WoodGood/blob/1.20/common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/TagUtility.java#L186
+
+                    ///REQUIRED: Add block to your mod's tab or Minecraft's tab
                     .setTabKey(yourModTab)
 
-                    /// Creating recipes for the block
+                    ///OPTIONAL: Creating recipes for the block
                     .defaultRecipe() // default: new ResourceLocation("twigs:oak_table") via recipes folder
-                    .addRecipe(modRes("path/to/recipeFile"))
+                    ///OPTIONAL: if the recipe has a different path unlike above
+                    .addRecipe(modRes("path/to/recipeFile")) // Do not use "recipes/"
 
-                    /// Special cases
+                    ///OPTIONAL: Special cases
                     .copyParentDrop() // copy the loot_table of the baseBlock (oak_table)
                     .copyParentTint() // Applying tinted color to Leaves - Good example is hedge from Quark OR Macaw's Fences & Walls
-                    .setRenderType(RenderLayer.CUTOUT)
+                    .setRenderType(RenderLayer.CUTOUT) //USAGE: CUTOUT, CUTOUT_MIPPED, SOLID, TRANSLUCENT
 
                     .build();
             this.addEntry(sampleBlock);
@@ -86,8 +98,39 @@ public class WoodGoodModule extends SimpleModule {
                     .addTexture(modRes("item/itemTexture"))
                     .addTag(new ResourceLocation("twigs:tables"), Registries.ITEM)
                     .setTabKey(tab)
+                    .defaultRecipe()
+                    .addRecipe(modRes("path/to/recipeFile"))
                     .build();
             this.addEntry(sampleItem);
 
+        }
+
+///      ┌──────────────────────────────────────────────────────────┐
+///      │     Manaully generating Textures or Modifying models     │
+///      └──────────────────────────────────────────────────────────┘
+        @Override
+        public void addDynamicClientResources(Consumer<ResourceGenTask> executor) {
+            super.addDynamicClientResources(executor);
+
+            executor.accept((manager, sink) -> {
+                /// a code to manually generate textures if `.addTexture()` doesn't generate a good looking texture
+                /// Good examples is RegionsUnexploredModule's shrub OR ChippedModule's bundledLog OR ValhelsiaStructuresModule's bundledPosts
+                /// You can look at above and other modules for references
+            });
+        }
+
+///      ┌──────────────────────────────────────────────────────────┐
+///      │  Manually generating Recipes or creating tags or adding  │
+///      │  blocks to tags                                          │
+///      └──────────────────────────────────────────────────────────┘
+        @Override
+        public void addDynamicServerResources(Consumer<ResourceGenTask> executor) {
+            super.addDynamicServerResources(executor);
+
+            executor.accept((manager, sink) -> {
+                /// Code to manually add tags to blocks or creating a new tags for recipes to be generated
+                /// `.addRecipe()` or `.defaultRecipe()` cannot generate recipes that use tags as ingredients
+                /// A good example is QuarkModule's hedge where its recipe is using tag as ingredient
+            });
         }
     }
