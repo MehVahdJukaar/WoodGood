2aa68e06cbac565e37984a932ddfbbf6b9a06322
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-14 23:10:05 +0200

    RE moduel in common

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/villagers_plus/VillagersPlusModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/villagers_plus/VillagersPlusModule.java
@@ -4,7 +4,10 @@ import com.lion.villagersplus.blocks.HorticulturistTableBlock;
 import com.lion.villagersplus.init.VPBlockEntities;
 import com.lion.villagersplus.init.VPBlocks;
 import com.lion.villagersplus.init.VPItemGroups;
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.mehvahdjukaar.every_compat.EveryCompat;
+import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
+import net.mehvahdjukaar.every_compat.api.PaletteStrategy;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.every_compat.misc.CompatSpritesHelper;
@@ -26,9 +29,9 @@ public class VillagersPlusModule extends SimpleModule {
                         w -> new HorticulturistTableBlock(BlockBehaviour.Properties.of().strength(0.5F).noOcclusion().ignitedByLava())
                 )
                 .requiresChildren("stripped_log") //REASON: recipes & textures
-                .createPaletteFromChild("stripped_log", CompatSpritesHelper.LOOKS_LIKE_SIDE_LOG_TEXTURE)
                 .addTextureM(modRes("block/oak_horticulturist_workstation"),
-                        EveryCompat.res("block/vp/oak_horticulturist_workstation_m"))
+                        EveryCompat.res("block/vp/oak_horticulturist_workstation_m"),
+                        PaletteStrategies.WOOD_STRIPPED_LOG_SIDE)
                 .addTile(VPBlockEntities.HORTICULTURIST_TABLE_BLOCK_ENTITY)
                 .setTabKey(VPItemGroups.ITEM_GROUP)
                 .defaultRecipe()
@@ -36,4 +39,5 @@ public class VillagersPlusModule extends SimpleModule {
         this.addEntry(tub);
 
     }
+
 }
--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/regions_unexplored/RegionsUnexploredModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/regions_unexplored/RegionsUnexploredModule.java
@@ -1,26 +1,10 @@
 package net.mehvahdjukaar.every_compat.modules.forge.regions_unexplored;
 
-import net.mehvahdjukaar.every_compat.EveryCompat;
-import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
-import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.every_compat.misc.CompatSpritesHelper;
-import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
-import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
-import net.mehvahdjukaar.moonlight.api.resources.textures.Palette;
-import net.mehvahdjukaar.moonlight.api.resources.textures.Respriter;
-import net.mehvahdjukaar.moonlight.api.resources.textures.TextureImage;
-import net.mehvahdjukaar.moonlight.api.resources.textures.TextureOps;
+import net.mehvahdjukaar.every_compat.modules.regions_unexplored.RegionsUnexploredModuleAbstract;
 import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesType;
-import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesTypeRegistry;
-import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
-import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.PushReaction;
@@ -28,178 +12,23 @@ import net.regions_unexplored.block.RuBlocks;
 import net.regions_unexplored.world.level.block.plant.branch.BranchBlock;
 import net.regions_unexplored.world.level.block.plant.tall.ShrubBlock;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.function.Consumer;
-
-import static net.mehvahdjukaar.every_compat.common_classes.TagUtility.createAndAddCustomTags;
-
 // SUPPORT: v0.5.6+
-public class RegionsUnexploredModule extends SimpleModule {
-    public final SimpleEntrySet<WoodType, Block> branchs;
-    public final SimpleEntrySet<LeavesType, Block> shrubs;
+public class RegionsUnexploredModule extends RegionsUnexploredModuleAbstract {
 
     public RegionsUnexploredModule(String modId) {
-        super(modId, "ru");
-        ResourceLocation tab = modRes("ru_main");
-
-        branchs = SimpleEntrySet.builder(WoodType.class, "branch",
-                        getModBlock("oak_branch"), () -> VanillaWoodTypes.OAK,
-                        w -> new BranchBlock(BlockBehaviour.Properties.copy(RuBlocks.ACACIA_BRANCH.get()), "branch")
-                )
-                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .addTag(modRes("branches_can_survive_on"), Registries.BLOCK)
-                .addTag(modRes("branches"), Registries.BLOCK)
-                .addTag(modRes("branches"), Registries.ITEM)
-                .setTabKey(tab)
-                .addRecipe(modRes("oak_branch_from_oak_log"))
-                .build();
-        this.addEntry(branchs);
-
-        shrubs = SimpleEntrySet.builder(LeavesType.class, "shrub",
-                        getModBlock("dark_oak_shrub"),
-                        () -> LeavesTypeRegistry.getValue("dark_oak"),
-                        l -> new ShrubBlock(Utils.copyPropertySafe(l.leaves).pushReaction(PushReaction.DESTROY)
-                                .ignitedByLava().noCollission().instabreak().sound(SoundType.AZALEA)
-                                .offsetType(BlockBehaviour.OffsetType.XZ))
-                )
-                .addCondition(l -> {
-                    boolean log = l.getWoodType() != null; //REASON: textures
-                    boolean sapling = l.getItemOfThis("sapling") != null; //REASON: recipes
-                    return log && sapling;
-                })
-                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .addTag(modRes("shrubs"), Registries.BLOCK)
-                .addTag(modRes("shrub_can_survive_on"), Registries.BLOCK)
-                .addTag(modRes("shrubs"), Registries.ITEM)
-                .setTabKey(tab)
-                .addRecipe(modRes("dark_oak_sapling_from_dark_oak_shrub"))
-                .addRecipe(modRes("dark_oak_shrub"))
-                .copyParentDrop()
-                .copyParentTint()
-                .build();
-        this.addEntry(shrubs);
+        super(modId);
     }
 
     @Override
-    public void onModSetup() {
-        branchs.blocks.forEach((woodType, block) -> ComposterBlock.COMPOSTABLES.put(block, 0.3F));
+    public Block newShrubBlock(LeavesType l) {
+        return new ShrubBlock(Utils.copyPropertySafe(l.leaves).pushReaction(PushReaction.DESTROY)
+                .ignitedByLava().noCollission().instabreak().sound(SoundType.AZALEA)
+                .offsetType(BlockBehaviour.OffsetType.XZ));
     }
 
     @Override
-    //TAGS
-    public void addDynamicServerResources(Consumer<ResourceGenTask> executor) {
-        super.addDynamicServerResources(executor);
-
-        executor.accept((manager, sink) -> {
-            for (WoodType woodType : WoodTypeRegistry.getTypes()) {
-                if (woodType.isVanilla() || woodType.getNamespace().equals("regions_unexplored")) continue;
-
-                //Tagging the planks as ingredient to get painted_planks
-                createAndAddCustomTags(new ResourceLocation("planks"), sink, woodType.planks);
-                createAndAddCustomTags(new ResourceLocation("forge:planks"), sink, woodType.planks);
-            }
-
-        });
-
+    public Block newBranchBlock(WoodType w) {
+        return new BranchBlock(BlockBehaviour.Properties.copy(RuBlocks.ACACIA_BRANCH.get()), "branch");
     }
 
-    @Override
-    //TEXTURES
-    public void addDynamicClientResources(Consumer<ResourceGenTask> executor) {
-        super.addDynamicClientResources(executor);
-
-// Generating branch textures ==========================================================================================
-        executor.accept((manager, sink) -> {
-            try (TextureImage branch_side = TextureImage.open(manager, EveryCompat.res("item/oak_branch_side"));
-                 TextureImage branch_top = TextureImage.open(manager, EveryCompat.res("item/oak_branch_top"));
-                 TextureImage branch_block = TextureImage.open(manager, modRes("block/oak_branch"))
-            ) {
-                Respriter respriterSIDE = Respriter.of(branch_side); // ITEM
-                Respriter respriterTOP = Respriter.of(branch_top); // ITEM
-                Respriter respriterBlock = Respriter.of(branch_block); // BLOCK
-
-                branchs.blocks.forEach((wood, block) -> {
-                    try (TextureImage logSide_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, wood.log, CompatSpritesHelper.LOOKS_LIKE_SIDE_LOG_TEXTURE));
-                         TextureImage logTop_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, wood.planks))) {
-
-                        String resLocITEM = "item/" + this.shortenedId() + "/" + wood.getAppendableId() + "_branch";
-                        String resLocBLOCK = "block/" + this.shortenedId() + "/" + wood.getAppendableId() + "_branch";
-
-                        List<Palette> list_logSide = Palette.fromAnimatedImage(logSide_texture);
-                        List<Palette> list_logTop = Palette.fromAnimatedImage(logTop_texture);
-
-                        // Block Texture
-                        sink.addTextureIfNotPresent(manager, resLocBLOCK, () -> respriterBlock.recolor(list_logSide));
-                        // Item Texture
-                        sink.addTextureIfNotPresent(manager, resLocITEM, () -> {
-                            TextureImage recoloredITEM = respriterSIDE.recolor(list_logSide);
-                            try (TextureImage recoloredTOP = respriterTOP.recolor(list_logTop)) {
-                                TextureOps.applyOverlay(recoloredITEM, recoloredTOP);
-                            }
-                            return recoloredITEM;
-                        });
-
-                    } catch (IOException e) {
-                        EveryCompat.LOGGER.error("Failed to get Log Texture for {} : {}", block, e);
-                    }
-                });
-            } catch (IOException e) {
-                EveryCompat.LOGGER.error("Failed to get Branch Item Texture for ", e);
-            }
-
-// Generating shrub textures ===========================================================================================
-            try (
-                    // middle is the bark part of shrub_top's
-                    TextureImage shrubTop = TextureImage.open(manager, modRes("block/dark_oak_shrub_top"));
-                    TextureImage shrubBottom = TextureImage.open(manager, modRes("block/dark_oak_shrub_bottom"));
-                    TextureImage shrubTopMask = TextureImage.open(manager, EveryCompat.res("block/ru/mask_shrub_top"));
-                    TextureImage shrubMiddleMask = TextureImage.open(manager, EveryCompat.res("block/ru/mask_shrub_middle"))
-            ) {
-
-                Respriter respriterBottom = Respriter.of(shrubBottom);
-
-                shrubs.blocks.forEach((leavesType, block) -> {
-                    String shrubPath = leavesType.createPathWith(shortenedId(), "shrub");
-
-                    // Generating textures for shrubs
-                    try (TextureImage logTexture = TextureImage.open(manager,
-                            RPUtils.findFirstBlockTextureLocation(manager, leavesType.getAssociatedWoodType().log,
-                                    CompatSpritesHelper.LOOKS_LIKE_SIDE_LOG_TEXTURE));
-                         TextureImage leavesTexture = TextureImage.open(manager,
-                                 RPUtils.findFirstBlockTextureLocation(manager, leavesType.leaves,
-                                         CompatSpritesHelper.LOOKS_LIKE_LEAF_TEXTURE))
-                    ) {
-                        Respriter respriterTop = Respriter.masked(shrubTop, shrubMiddleMask);
-
-                        List<Palette> logSidePalette = Palette.fromAnimatedImage(logTexture);
-                        List<Palette> leavesPalette = Palette.fromAnimatedImage(leavesTexture);
-
-                        // Adding to the resource
-                        String resLoc = "block/" + shrubPath;
-                        sink.addTextureIfNotPresent(manager, resLoc + "_bottom", () -> {
-                            // Recoloring the shrub's Bottom
-                            return respriterBottom.recolor(logSidePalette);
-                        });
-
-                        sink.addTextureIfNotPresent(manager, resLoc + "_top", () -> {
-                            // Recoloring the shrub's Top (the leaves part)
-                            try (TextureImage recoloredShrubTop = respriterTop.recolor(leavesPalette)) {
-                                // Recoloring the shrub's Middle (the bark part)
-                                Respriter respriterMiddle = Respriter.masked(recoloredShrubTop, shrubTopMask);
-                                return respriterMiddle.recolor(logSidePalette);
-                            }
-                        });
-
-
-                    } catch (IOException e) {
-                        EveryCompat.LOGGER.error("Failed to get texture for {} : {}", block.toString(), e.getMessage());
-                    }
-                });
-            } catch (IOException e) {
-                EveryCompat.LOGGER.error("Failed to open textures for: {}", e.getMessage());
-            }
-
-        });
-    }
 }
