7fc6c0b4db20fb3393a13ef394bf6f2cab16147d
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-14 23:48:40 +0200

    trigs

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/twigs/TwigsModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/twigs/TwigsModule.java
@@ -1,10 +1,7 @@
 package net.mehvahdjukaar.every_compat.modules.twigs;
 
 import com.ninni.twigs.block.TableBlock;
-import net.mehvahdjukaar.every_compat.api.RenderLayer;
-import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
-import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.every_compat.api.TabAddMode;
+import net.mehvahdjukaar.every_compat.api.*;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
@@ -30,10 +27,9 @@ public class TwigsModule extends SimpleModule {
                 .setTabKey(modRes("twig"))
                 .setTabMode(TabAddMode.AFTER_SAME_TYPE)
                 .setRenderType(RenderLayer.CUTOUT_MIPPED)
-                .createPaletteFromPlanks((p) -> p.remove(p.getDarkest()))
-                .addTexture(modRes("block/oak_table"))
-                .addTexture(modRes("block/oak_table_top"))
-                .addTexture(modRes("block/oak_table_bottom"))
+                .addTexture(modRes("block/oak_table"), PaletteStrategies.WOOD_PLANKS_REMOVE_DARKEST)
+                .addTexture(modRes("block/oak_table_top"), PaletteStrategies.WOOD_PLANKS_REMOVE_DARKEST)
+                .addTexture(modRes("block/oak_table_bottom"), PaletteStrategies.WOOD_PLANKS_REMOVE_DARKEST)
                 .build();
 
         this.addEntry(tables);
