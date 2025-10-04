2026daadaa9c96e29e28b51eb6839df613f7df3f
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-10-04 02:50:02 +0200

    experimental mega ugly code to prevent classloading bullshit

--- common/src/main/java/net/mehvahdjukaar/every_compat/api/EveryCompatAPI.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/api/EveryCompatAPI.java
@@ -1,10 +1,12 @@
 package net.mehvahdjukaar.every_compat.api;
 
+import net.mehvahdjukaar.every_compat.EcTempPluginStorage;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.example.WoodGoodAddonExample;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Use this class register new wood type blocks and module to Every Compat
@@ -22,9 +24,15 @@ public class EveryCompatAPI {
      * Take a look at {@link net.mehvahdjukaar.every_compat.api.example.WoodGoodModuleExample} for an example module
      */
     public static synchronized void registerModule(CompatModule module) {
-        EveryCompat.addModule(module);
+        EcTempPluginStorage.add(module);
     }
 
+    public static synchronized void registerOptionalModule(String modId, Supplier<Class<? extends CompatModule>> moduleClass) {
+        EcTempPluginStorage.addOptional(modId, moduleClass);
+    }
+
+    //no need for this
+    @Deprecated(forRemoval = true)
     public static Collection<CompatModule> getModule(String modId) {
         return EveryCompat.getModulesOfMod(modId);
     }
@@ -32,7 +40,7 @@ public class EveryCompatAPI {
     /// If you mod has compat mods that support it with Biomes O' Plenty or other Wood Mods below can make an exception
     /// so EC won't generate blocks from your mod with Biomes O' Plenty
     public static void addOtherCompatMod(String compatModId, List<String> fromModId, List<String> supportedModId){
-        EveryCompat.addOtherCompatMod(compatModId, fromModId, supportedModId);
+        EcTempPluginStorage.addOtherCompatMod(compatModId, fromModId, supportedModId);
     }
 
 
