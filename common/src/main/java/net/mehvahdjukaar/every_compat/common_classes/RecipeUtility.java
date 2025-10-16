02e1f25d49fd504edfe888cfa858ce18ab808aac
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-10-15 21:02:19 -0600

    RecipeUtility: Changed the logging to have just "@"

--- common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/RecipeUtility.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/common_classes/RecipeUtility.java
@@ -26,7 +26,7 @@ public class RecipeUtility {
                                                  ResourceLocation newRecipeLoc, ResourceSink sink, ResourceManager manager) {
         if (Objects.nonNull(output)) {
             try (InputStream recipeStream = manager.getResource(recipeLoc)
-                    .orElseThrow(() -> new FileNotFoundException("File Not Found: " + recipeLoc)).open()) {
+                    .orElseThrow(() -> new FileNotFoundException(" @ " + recipeLoc)).open()) {
                 JsonObject recipe = RPUtils.deserializeJson(recipeStream);
 
                 // Editing the recipe
@@ -50,7 +50,7 @@ public class RecipeUtility {
                                            ResourceSink sink, ResourceManager manager) {
         if (Objects.nonNull(newResult)) {
             try (InputStream recipeStream = manager.getResource(ResType.RECIPES.getPath(recipeLoc))
-                    .orElseThrow(() -> new FileNotFoundException("File Not Found: " + recipeLoc)).open()) {
+                    .orElseThrow(() -> new FileNotFoundException(" @ " + recipeLoc)).open()) {
                 JsonObject recipe = RPUtils.deserializeJson(recipeStream);
 
                 // Editing the recipe
