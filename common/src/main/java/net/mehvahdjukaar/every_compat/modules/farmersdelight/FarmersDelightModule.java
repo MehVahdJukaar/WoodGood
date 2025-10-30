2d29dbc511a096658669c508399eb3d22fd9042f
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-10-30 01:07:55 -0600

    FarmersDelightModule: Added COMPAT_RECIPE_MODS

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/farmersdelight/FarmersDelightModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/farmersdelight/FarmersDelightModule.java
@@ -5,12 +5,15 @@ import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.every_compat.api.TabAddMode;
+import net.mehvahdjukaar.every_compat.misc.HardcodedBlockType;
+import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
 import net.mehvahdjukaar.moonlight.api.resources.ResType;
 import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
 import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceSink;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
@@ -22,8 +25,11 @@ import vectorwing.farmersdelight.common.block.CabinetBlock;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
+
+import static java.util.Map.entry;
 
 // SUPPORT: FABRIC-v2.4.0+ | FORGE-v1.2.8+
 public class FarmersDelightModule extends SimpleModule {
@@ -71,22 +77,28 @@ public class FarmersDelightModule extends SimpleModule {
 
         executor.accept((manager, handler) -> {
             // Creating cutting_board recipes
-            cabinets.items.forEach(((woodType, item) -> {
+            for (WoodType woodType : WoodTypeRegistry.INSTANCE) {
+                if (HardcodedBlockType.isKnownVanillaWood(woodType)) continue;
 
-                createCuttingRecipe("door", woodType.getBlockOfThis("door"), woodType.planks,
-                        woodType, handler, manager);
-                createCuttingRecipe("hanging_sign", woodType.getBlockOfThis("hanging_sign"), woodType.planks,
-                        woodType, handler, manager);
-                createCuttingRecipe("sign", woodType.getBlockOfThis("sign"), woodType.planks,
-                        woodType, handler, manager);
-                createCuttingRecipe("trapdoor", woodType.getBlockOfThis("trapdoor"), woodType.planks,
-                        woodType, handler, manager);
-                createCuttingRecipe("log", woodType.log, woodType.getBlockOfThis("stripped_log"),
-                        woodType, handler, manager);
-                createCuttingRecipe("wood", woodType.getBlockOfThis("wood"), woodType.getBlockOfThis("stripped_wood"),
-                        woodType, handler, manager);
+                // Skip if one of Farmer's-Cutting mods is installed
+                String namespaceRegex = COMPAT_RECIPE_MODS.getOrDefault(woodType.getNamespace(), "none");
+                boolean isRecipeModNotInstalled = !PlatHelper.getInstalledMods().contains(namespaceRegex);
 
-            }));
+                if (isRecipeModNotInstalled) {
+                    createCuttingRecipe("door", woodType.getBlockOfThis("door"), woodType.planks,
+                            woodType, handler, manager);
+                    createCuttingRecipe("hanging_sign", woodType.getBlockOfThis("hanging_sign"), woodType.planks,
+                            woodType, handler, manager);
+                    createCuttingRecipe("sign", woodType.getBlockOfThis("sign"), woodType.planks,
+                            woodType, handler, manager);
+                    createCuttingRecipe("trapdoor", woodType.getBlockOfThis("trapdoor"), woodType.planks,
+                            woodType, handler, manager);
+                    createCuttingRecipe("log", woodType.log, woodType.getBlockOfThis("stripped_log"),
+                            woodType, handler, manager);
+                    createCuttingRecipe("wood", woodType.getBlockOfThis("wood"), woodType.getBlockOfThis("stripped_wood"),
+                            woodType, handler, manager);
+                }
+            }
         });
     }
 
@@ -116,4 +128,23 @@ public class FarmersDelightModule extends SimpleModule {
             }
         }
     }
+
+    // a recipe mod, not full Compat-Mod providing cutting-board recipes for other Wood-Mods
+    // farmers-cutting-collection.*.jar
+    private final Map<String, String> COMPAT_RECIPE_MODS = Map.ofEntries(
+        entry("aether", "fcaether"),
+        entry("betterend", "fcbe"),
+        entry("betternether", "fcbn"),
+        entry("biomesoplenty", "fcbop"),
+        entry("biomeswevegone", "fcbwg"),
+        entry("blue_skies", "fcbs"),
+        entry("cinderscapes", "fccs"),
+        entry("eternal_starlight", "fces"),
+        entry("natures_spirit", "fcns"),
+        entry("nethers_exoticism", "fcne"),
+        entry("promenade", "fcpromenade"),
+        entry("regions_unexplored", "fcru"),
+        entry("terrestria", "fcterrestria"),
+        entry("twilightforest", "fctf")
+    );
 }
