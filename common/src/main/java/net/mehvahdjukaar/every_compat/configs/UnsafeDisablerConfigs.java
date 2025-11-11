437478e41743b43b2f1ba66dc8106c19ddb9a768
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-11-11 15:58:13 -0700

    UnsafeDisablerConfigs & HardcodedBlockType: Updated (Ported from 1.21)

--- common/src/main/java/net/mehvahdjukaar/every_compat/configs/UnsafeDisablerConfigs.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/configs/UnsafeDisablerConfigs.java
@@ -12,6 +12,7 @@ public class UnsafeDisablerConfigs {
 
     public static final Supplier<List<String>> WOOD_TYPES_BLACKLIST;
     public static final Supplier<List<String>> LEAVES_TYPES_BLACKLIST;
+    public static final Supplier<List<String>> BLOCKS_BLACKLIST;
     public static final Supplier<List<String>> ENTRY_SETS_BLACKLIST;
     public static final Supplier<List<String>> MODULES_BLACKLIST;
     public static final Supplier<Boolean> INCLUDE_ALL_WOOD_MODULES;
@@ -39,6 +40,8 @@ public class UnsafeDisablerConfigs {
     
                     Module - is a Supported Mod, just a modId is sufficient.
                     EntrySet - is a FurnitureType or DecorativeType that Wood-Good is supporting via the mod. it is either block or item.
+                    Wood-Mods - Biomes O' Plenty, The Twilight Forest, so on...
+                    Supported-Mods - The mods that EveryCompat is currently supporting
     
                     NOTE: blacklisting a Module will be applied to Wood-Good, Stone-Zone, Gems-Realm
                 """;
@@ -59,6 +62,20 @@ public class UnsafeDisablerConfigs {
         LEAVES_TYPES_BLACKLIST = builder.comment("Exclude LeavesType from all of Modules\n\tThe example is same as WoodType's").define("blacklist", List.of());
         builder.pop();
 
+        builder.push("block");
+        String blockExample = """
+                    This is only applied to Wood-Good.
+                    EXAMPLE: blacklist = [
+                        "chipped/biomesoplenty/checkered_redwood_trapdoor",\tCOMMENT: excluded a checkered_trapdoor from Chipped with Biomes-O'-Plenty's redwood
+                        "variantvanillablocks/biomesoplenty/fir_chest",\t\tCOMMENT: excluded a chest from Variant-Vanilla-Blocks with Biomes-O'-Plenty's fir
+                        ".*/biomesoplenty/fir_chest",\t\t\t\t\t\t\tCOMMENT: .* is an RegEx, so it exclude fir_chest from any Supported-Mods with just Biomes-O'-Plenty (Wood-Mods)
+                        "chipped/.*/fir_chest",\t\t\t\t\t\t\t\tCOMMENT: .* is an RegEx, so it exclude fir_chest from any Wood-Mods with just chipped (Supported-Mod)
+                        ".*fir_chest",\t\t\t\t\t\t\t\t\t\t\tCOMMENT: .* is an RegEx, so it exclude fir_chest from any Supported-Mods & Wood-Mods
+                    ]
+                """;
+        BLOCKS_BLACKLIST = builder.comment("Exclude a specific WoodType/LeavesType block\n"+blockExample).define("blacklist", List.of());
+        builder.pop();
+
         builder.push("entryset");
         String entrysetExample = """
                     This is only applied to Wood-Good.
@@ -67,6 +84,7 @@ public class UnsafeDisablerConfigs {
                         "variantvanillablocks:chest",\t\tCOMMENT: variantvanillablocks:oak_chest without "oak"
                         "chipped:.*"\t\t\t\t\t\tCOMMENT: .* is an regex which will exclude all of EntrySets from one Module - Wood-Good ONLY
                     ]
+                    NOTE: This excluded one entryset from all WoodTypes/LeavesTypes
                 """;
         ENTRY_SETS_BLACKLIST = builder.comment("Exclude EntrySet from the module for All of WoodType or LeavesType\n"+entrysetExample).define("blacklist", List.of());
         builder.pop();
@@ -93,7 +111,8 @@ public class UnsafeDisablerConfigs {
         CONFIG_SPEC.loadFromFile();
 
         // Warning Message
-        if (!WOOD_TYPES_BLACKLIST.get().isEmpty() || !LEAVES_TYPES_BLACKLIST.get().isEmpty() || !ENTRY_SETS_BLACKLIST.get().isEmpty() || !MODULES_BLACKLIST.get().isEmpty()) {
+        if (!WOOD_TYPES_BLACKLIST.get().isEmpty() || !LEAVES_TYPES_BLACKLIST.get().isEmpty() || !BLOCKS_BLACKLIST.get().isEmpty()
+                || !ENTRY_SETS_BLACKLIST.get().isEmpty() || !MODULES_BLACKLIST.get().isEmpty()) {
             EveryCompat.LOGGER.warn("""
                             \n
                             ===========================================================
