97528a1c1ffc47f7a05e5ea155bac8896cf7710f
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-11-19 15:52:32 -0700

    UtilityTag: Renamed "pack" to "sink"

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/UtilityTag.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/UtilityTag.java
@@ -125,7 +125,7 @@ public class UtilityTag {
      */
     public static <T extends BlockType, B extends Block> void addTagToAllBlocks(
             Map<T, B> blocks, String nameBlockTypeOrRegEx, String fromModId,
-            String tagResLoc, boolean includeBlock, boolean includeItem, ResourceSink pack,
+            String tagResLoc, boolean includeBlock, boolean includeItem, ResourceSink sink,
             @Nullable String regexBlockId
     ) {
         if (PlatHelper.isModLoaded(fromModId) || fromModId.isEmpty()) {
@@ -148,8 +148,8 @@ public class UtilityTag {
                 }
             }
             if (isTagCreated) {
-                if (includeBlock) pack.addTag(tagBuilder, Registries.BLOCK);
-                if (includeItem) pack.addTag(tagBuilder, Registries.ITEM);
+                if (includeBlock) sink.addTag(tagBuilder, Registries.BLOCK);
+                if (includeItem) sink.addTag(tagBuilder, Registries.ITEM);
             }
         }
     }
