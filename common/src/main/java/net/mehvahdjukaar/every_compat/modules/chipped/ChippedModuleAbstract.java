75b42cf30308f28e4dfaac2f1bc81e1a9a861ae7
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-12-15 02:14:18 -0700

    ChippedModule: Renamed & Removed unneeded codes in Abstract class

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/chipped/ChippedModuleAbstract.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/chipped/ChippedModuleAbstract.java
@@ -3,77 +3,24 @@ package net.mehvahdjukaar.every_compat.modules.chipped;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.mehvahdjukaar.every_compat.EveryCompat;
-import net.mehvahdjukaar.every_compat.api.EntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.every_compat.misc.HardcodedBlockType;
 import net.mehvahdjukaar.moonlight.api.resources.ResType;
 import net.mehvahdjukaar.moonlight.api.resources.SimpleTagBuilder;
-import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
 import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceSink;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
-import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-
-import java.util.List;
-import java.util.function.Consumer;
 
 //SUPPORT: v4.0.2+
-public class ChippedAbstractModule extends SimpleModule {
+public class ChippedModuleAbstract extends SimpleModule {
 
     public static String tabPath = "main";
 
-    public ChippedAbstractModule(String modId) {
+    public ChippedModuleAbstract(String modId) {
         super(modId, "ch", EveryCompat.MOD_ID);
     }
-
-    @Override
-    // RECIPES, LOOT_TABLES
-    public void addDynamicServerResources(Consumer<ResourceGenTask> executor) {
-        super.addDynamicServerResources(executor);
-
-        executor.accept((manager, handler)-> {
-            // use this. also set the entry to no drop so we don't have 2.
-            // why do we need this instead of copy parent drop? macaw has doors too and they work
-            // chipped adds their loot not via loot table. this is why we need this. no other mod should need this stuff
-            // this shouldnt be needed.... why isnt copy parent loot working?
-            List<EntrySet<?>> doors = this.getEntries().stream().filter(
-                    e -> e.getName().contains("door") && !e.getName().contains("trapdoor")).toList();
-            for (var e : doors) {
-                if (e instanceof SimpleEntrySet<?, ?> se) {
-                    for (var d : se.blocks.values()) {
-                        handler.addLootTable(d, createDoorLoot(d));
-                    }
-                }
-            }
-
-            addCarpenterRecipe(handler, "planks");
-            addCarpenterRecipe(handler, "door");
-            addCarpenterRecipe(handler, "trapdoor");
-            addCarpenterRecipe(handler, "log");
-            addCarpenterRecipe(handler, "stripped_log");
-        });
-    }
-
-    public static LootTable.Builder createDoorLoot(Block block) {
-        return LootTable.lootTable().withPool(
-                LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1.0F))
-                        .add(LootItem.lootTableItem(block)
-                                .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
-                                        .setProperties(StatePropertiesPredicate.Builder.properties()
-                                                .hasProperty(DoorBlock.HALF, DoubleBlockHalf.LOWER)))));
-    }
-
 
     @SuppressWarnings("SameParameterValue")
     protected void addCarpenterRecipe(ResourceSink pack, String identifier) {
