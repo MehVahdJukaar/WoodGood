d546d9e48e658edb521b7474a13d25a1c70928a0
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-01-08 15:17:27 -0700

    RegionsUnexploredModule: Updated to support v0.5.7 & Fix Crash, #1160

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/regions_unexplored/RegionsUnexploredModule.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/modules/fabric/regions_unexplored/RegionsUnexploredModule.java
@@ -3,16 +3,11 @@ package net.mehvahdjukaar.every_compat.modules.fabric.regions_unexplored;
 import net.mehvahdjukaar.every_compat.modules.regions_unexplored.RegionsUnexploredModuleAbstract;
 import net.mehvahdjukaar.moonlight.api.set.leaves.LeavesType;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.PushReaction;
-import net.regions_unexplored.block.RuBlocks;
 import net.regions_unexplored.world.level.block.plant.branch.BranchBlock;
 import net.regions_unexplored.world.level.block.plant.tall.ShrubBlock;
 
-// SUPPORT: v0.5.6+
+//See MacawFurnitureAbstractModule's SUPPORTED VERSION
 public class RegionsUnexploredModule extends RegionsUnexploredModuleAbstract {
 
     public RegionsUnexploredModule(String modId) {
@@ -21,14 +16,12 @@ public class RegionsUnexploredModule extends RegionsUnexploredModuleAbstract {
 
     @Override
     public Block newShrubBlock(LeavesType l) {
-        return new ShrubBlock(Utils.copyPropertySafe(l.leaves).pushReaction(PushReaction.DESTROY)
-                .ignitedByLava().noCollission().instabreak().sound(SoundType.AZALEA)
-                .offsetType(BlockBehaviour.OffsetType.XZ));
+        return new ShrubBlock(SHRUB_PROPERTIES);
     }
 
     @Override
     public Block newBranchBlock(WoodType w) {
-        return new BranchBlock(BlockBehaviour.Properties.ofFullCopy(RuBlocks.ACACIA_BRANCH.get()), BranchBlock.BranchType.BRANCH);
+        return new BranchBlock(BRANCH_PROPERTIES, BranchBlock.BranchType.BRANCH);
     }
 
 }
