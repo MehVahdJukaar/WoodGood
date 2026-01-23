1b1d30a8ff5fe919571883e4bcf5d65326e0b86f
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-01-23 15:07:04 -0700

    BackpackedModule: Updated changed code & to support v3.0.0+, CLOSED #1165

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/mrcrayfish/BackpackedModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/mrcrayfish/BackpackedModule.java
@@ -3,6 +3,7 @@ package net.mehvahdjukaar.every_compat.modules.mrcrayfish;
 import com.mrcrayfish.backpacked.block.ShelfBlock;
 import com.mrcrayfish.backpacked.core.ModBlockEntities;
 import com.mrcrayfish.backpacked.core.ModBlocks;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
@@ -13,13 +14,13 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 
-//SUPPORT: v2.2.3+
+//SUPPORT: v3.0.0+
 public class BackpackedModule extends SimpleModule {
 
     public final SimpleEntrySet<WoodType, Block> shelves;
 
     public BackpackedModule(String modId) {
-        super(modId, "bp");
+        super(modId, "bp", EveryCompat.MOD_ID);
 
         shelves = SimpleEntrySet.builder(WoodType.class, "backpack_shelf",
                         ModBlocks.OAK_BACKPACK_SHELF::get, () -> VanillaWoodTypes.OAK,
@@ -28,7 +29,7 @@ public class BackpackedModule extends SimpleModule {
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .setTabKey(modRes("creative_tab"))
                 .addRecipe(modRes("oak_backpack_shelf"))
-                .addTile(ModBlockEntities.SHELF::get)
+                .addTile(ModBlockEntities.BACKPACK_SHELF::get)
                 .setRenderType(RenderLayer.CUTOUT)
                 .build();
         this.addEntry(shelves);
