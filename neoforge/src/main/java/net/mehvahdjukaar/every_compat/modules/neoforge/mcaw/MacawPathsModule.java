2bf52307161e8e53b7fc798f82d5a2b8d928ada3
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-03-27 18:48:07 -0600

    MacawPathsModule: Updated & Added an Abstract class

--- neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/mcaw/MacawPathsModule.java
+++ neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/mcaw/MacawPathsModule.java
@@ -1,36 +1,20 @@
 package net.mehvahdjukaar.every_compat.modules.neoforge.mcaw;
 
-import com.mcwpaths.kikoz.init.BlockInit;
 import com.mcwpaths.kikoz.objects.FacingPathBlock;
-import net.mehvahdjukaar.every_compat.api.RenderLayer;
-import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
-import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
-import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
+import net.mehvahdjukaar.every_compat.modules.macaw.MacawPathsModuleAbstract;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 
-//SUPPORT v1.0.4+
-public class MacawPathsModule extends EveryCompatModule {
-
-    public final SimpleEntrySet<WoodType, Block> PLANKS_PATHS;
+//See MacawPathModuleAbstract's SUPPORTED VERSION
+public class MacawPathsModule extends MacawPathsModuleAbstract {
 
     public MacawPathsModule(String modId) {
-        super(modId, "mcp");
+        super(modId);
+    }
 
-        PLANKS_PATHS = SimpleEntrySet.builder(WoodType.class, "planks_path",
-                        BlockInit.OAK_PLANKS_PATH, () -> VanillaWoodTypes.OAK,
-                        w -> new FacingPathBlock(Utils.copyPropertySafe(w.planks))
-                )
-                .addTexture(modRes("block/oak_planks_path"))
-                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .setTab(getModTab(
-"pathsitemgroup"))
-                .setRenderType(RenderLayer.CUTOUT_MIPPED)
-                .defaultRecipe()
-                .build();
-        this.addEntry(PLANKS_PATHS);
+    @Override
+    public Block newFacingPathBlock(WoodType woodType) {
+        return new FacingPathBlock(Utils.copyPropertySafe(woodType.planks));
     }
 }
