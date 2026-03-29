2ace90756b69d6e797e5f47729af8a41056ddf7d
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-03-28 21:56:56 -0600

    TwilightForestModule: Reorganized & Added hollow_log (item)

--- neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/twilightforest/TwilightForestModule.java
+++ neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/twilightforest/TwilightForestModule.java
@@ -1,10 +1,10 @@
 package net.mehvahdjukaar.every_compat.modules.neoforge.twilightforest;
 
 import net.mehvahdjukaar.every_compat.EveryCompat;
+import net.mehvahdjukaar.every_compat.api.ItemOnlyEntrySet;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
-import net.mehvahdjukaar.moonlight.api.misc.Registrator;
 import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
@@ -33,9 +33,10 @@ import static net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodChildKeys.STRI
 public class TwilightForestModule extends EveryCompatModule {
 
     public final SimpleEntrySet<WoodType, BanisterBlock> banisters;
-    public final SimpleEntrySet<WoodType, VerticalHollowLogBlock> hollowLogsVertical;
-    public final SimpleEntrySet<WoodType, HorizontalHollowLogBlock> hollowLogsHorizontal;
-    public final SimpleEntrySet<WoodType, ClimbableHollowLogBlock> hollowLogsClimbable;
+    public final SimpleEntrySet<WoodType, VerticalHollowLogBlock> hollow_log_vertical;
+    public final SimpleEntrySet<WoodType, HorizontalHollowLogBlock> hollow_log_horizontal;
+    public final SimpleEntrySet<WoodType, ClimbableHollowLogBlock> hollow_log_climbable;
+    public final ItemOnlyEntrySet<WoodType, Item> hollow_log;
 
     public TwilightForestModule(String modId) {
         super(modId, "tf");
@@ -54,19 +55,19 @@ public class TwilightForestModule extends EveryCompatModule {
                 .build();
         this.addEntry(banisters);
 
-        hollowLogsHorizontal = SimpleEntrySet.builder(WoodType.class, "log_horizontal", "hollow",
+        hollow_log_horizontal = SimpleEntrySet.builder(WoodType.class, "log_horizontal", "hollow",
                         TFBlocks.HOLLOW_BIRCH_LOG_HORIZONTAL, () -> VanillaWoodTypes.BIRCH,
                         w -> new HorizontalHollowLogBlock(Utils.copyPropertySafe(w.log))
                 )
                 .requiresChildren(STRIPPED_LOG) //REASON: Textures
                 //TEXTURES: stripped_log
                 .addTag(modRes("hollow_logs_horizontal"), Registries.BLOCK)
-                .noItem().noTab() //REASON: it's using the hollowLogsVertical's tab/item as the main
+                .noItem().noTab() //REASON: it's using the hollow_log's tab/item as the main
                 .setRenderType(RenderLayer.CUTOUT_MIPPED)
                 .build();
-        this.addEntry(hollowLogsHorizontal);
+        this.addEntry(hollow_log_horizontal);
 
-        hollowLogsVertical = SimpleEntrySet.builder(WoodType.class, "log_vertical", "hollow",
+        hollow_log_vertical = SimpleEntrySet.builder(WoodType.class, "log_vertical", "hollow",
                         TFBlocks.HOLLOW_BIRCH_LOG_VERTICAL, () -> VanillaWoodTypes.BIRCH,
                         w -> {
                             var id = EveryCompat.res(this.shortenedId() + "/" + w.getVariantId("hollow", true) + "_log_climbable");
@@ -75,43 +76,42 @@ public class TwilightForestModule extends EveryCompatModule {
                 .requiresChildren(STRIPPED_LOG) //REASON: Textures
                 //TEXTURES: stripped_log
                 .addTag(modRes("hollow_logs_vertical"), Registries.BLOCK)
-                .noItem()
-                .setTab(tab)
+                .noItem().noTab() //REASON: it's using the hollow_log's tab/item as the main
                 .addRecipe(modRes("stonecutting/birch_log/hollow_birch_log"))
                 .build();
-        this.addEntry(hollowLogsVertical);
+        this.addEntry(hollow_log_vertical);
 
-        hollowLogsClimbable = SimpleEntrySet.builder(WoodType.class, "log_climbable", "hollow",
+        hollow_log_climbable = SimpleEntrySet.builder(WoodType.class, "log_climbable", "hollow",
                         TFBlocks.HOLLOW_BIRCH_LOG_CLIMBABLE, () -> VanillaWoodTypes.BIRCH,
                         w -> new ClimbableHollowLogBlock(
-                                DeferredHolder.create(Registries.BLOCK, Utils.getID(hollowLogsVertical.blocks.get(w))),
+                                DeferredHolder.create(Registries.BLOCK, Utils.getID(hollow_log_vertical.blocks.get(w))),
                                 Utils.copyPropertySafe(w.log))
                 )
                 .requiresChildren(STRIPPED_LOG) //REASON: Textures
                 //TEXTURES: stripped_log
                 .addTag(modRes("hollow_logs_climbable"), Registries.BLOCK)
-                .noItem().noTab() //REASON: it's using the hollowLogsVertical's tab/item as the main
+                .noItem().noTab() //REASON: it's using the hollow_log's tab/item as the main
                 .setRenderType(RenderLayer.CUTOUT_MIPPED)
                 .build();
-        this.addEntry(hollowLogsClimbable);
+        this.addEntry(hollow_log_climbable);
 
-    }
+        hollow_log = ItemOnlyEntrySet.builder(WoodType.class, "log", "hollow",
+                        getModItem("hollow_birch_log"), () -> VanillaWoodTypes.BIRCH,
+                        w -> new HollowLogItem(
+                                DeferredHolder.create(Registries.BLOCK, Utils.getID(hollow_log_horizontal.blocks.get(w))),
+                                DeferredHolder.create(Registries.BLOCK, Utils.getID(hollow_log_vertical.blocks.get(w))),
+                                DeferredHolder.create(Registries.BLOCK, Utils.getID(hollow_log_climbable.blocks.get(w))),
+                                new Item.Properties())
+                )
+                .requiresFromMap(hollow_log_vertical.blocks)
+                .requiresFromMap(hollow_log_horizontal.blocks)
+                .requiresFromMap(hollow_log_climbable.blocks)
+                .setTab(tab)
+                //TEXTURES: stripped_log
+                .addTag(modRes("hollow_logs_climbable"), Registries.BLOCK)
+                .build();
+        this.addEntry(hollow_log);
 
-    @Override
-    public void registerItems(Registrator<Item> registry) {
-        super.registerItems(registry);
-        hollowLogsVertical.blocks.forEach((w, b) -> {
-            String itemName = Utils.getID(b).getPath().replace("_vertical", "");
-            String childKey = this.getModId() + ":hollow_log";
-            Item i = new HollowLogItem(
-                    DeferredHolder.create(Registries.BLOCK, EveryCompat.res(itemName + "_horizontal")),
-                    DeferredHolder.create(Registries.BLOCK, Utils.getID(b)),
-                    DeferredHolder.create(Registries.BLOCK, EveryCompat.res(itemName + "_climbable")),
-                    new Item.Properties());
-            hollowLogsVertical.items.put(w, i);
-            w.addChild(childKey, i);
-            registry.register(EveryCompat.res(itemName), i);
-        });
     }
 
     @Override
@@ -120,11 +120,11 @@ public class TwilightForestModule extends EveryCompatModule {
                 (s, l, pos, i) -> s.getValue(ClimbableHollowLogBlock.VARIANT) != HollowLogVariants.Climbable.VINE ? -1 :
                         l != null && pos != null ?
                                 BiomeColors.getAverageFoliageColor(l, pos) : FoliageColor.getDefaultColor(),
-                hollowLogsClimbable.blocks.values().toArray(Block[]::new));
+                hollow_log_climbable.blocks.values().toArray(Block[]::new));
         event.register(
                 (s, l, pos, i) -> l != null && pos != null ?
                         BiomeColors.getAverageGrassColor(l, pos) : -1,
-                hollowLogsHorizontal.blocks.values().toArray(Block[]::new));
+                hollow_log_horizontal.blocks.values().toArray(Block[]::new));
     }
 
 }
