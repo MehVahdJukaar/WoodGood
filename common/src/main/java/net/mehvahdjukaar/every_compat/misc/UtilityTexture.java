203b187b19241a940eec674dd95f832fe0470267
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-06-06 18:42:22 -0600

    BoatLoadModule: Fixed the placed large_boat's missing textures & CLOSED #1269

--- common/src/main/java/net/mehvahdjukaar/every_compat/misc/UtilityTexture.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/misc/UtilityTexture.java
@@ -223,6 +223,7 @@ public class UtilityTexture {
 
             //unfortunately theres no programmatic way to get all boat textures of a given boat. also because entities might be for multiple wod types. we can only generate them from scratc
             boats.items.forEach((woodType, boat) -> {
+                //NOTE: folderDepth is set to 3 so it can include entity/
                 var newPath = BlockTypeResTransformer.replaceFullGenericType(baseTextureLoc.getPath(), woodType, Utils.getID(boat), "oak", null, 2);
 
                 sink.addTextureIfNotPresent(manager, newPath, () -> {
@@ -260,7 +261,7 @@ public class UtilityTexture {
 
             boats.items.forEach((woodType, boat) -> {
 
-                var newPath = BlockTypeResTransformer.replaceFullGenericType(baseTextureLoc.getPath(), woodType, Utils.getID(boat), "oak", null, 1);
+                var newPath = BlockTypeResTransformer.replaceTypeNoNamespace(baseTextureLoc.getPath(), woodType, Utils.getID(boat), "oak");
 
                 sink.addTextureIfNotPresent(manager, newPath,
                         () -> createBoatItemTexture(manager, woodType, respriter)
--- forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/abnormal/BoatLoadModule.java
+++ forge/src/main/java/net/mehvahdjukaar/every_compat/modules/forge/abnormal/BoatLoadModule.java
@@ -22,6 +22,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+import static net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodChildKeys.*;
+
 //SUPPORT: v5.0.1+
 public class BoatLoadModule extends SimpleModule {
     public final ItemOnlyEntrySet<WoodType, Item> largeBoats;
@@ -35,44 +37,39 @@ public class BoatLoadModule extends SimpleModule {
         largeBoats = ItemOnlyEntrySet.builder(WoodType.class, "boat", "large",
                         getModItem("large_oak_boat"),
                         () -> VanillaWoodTypes.OAK,
-                        w -> new LargeBoatItem(getBoatType(w))
+                        woodType -> new LargeBoatItem(getBoatType(woodType))
                 )
                 .setTabKey(tab)
                 .requiresChildren(VanillaWoodChildKeys.BOAT) //REASON: recipes
                 .addTag(ItemTags.BOATS, Registries.ITEM)
                 .addTag(modRes("large_boats"), Registries.ITEM)
                 .addRecipe(modRes("large_oak_boat"))
-//                .addTexture(modRes("entity/large_boat/oak"))
-//                .addTextureM(modRes("entity/furnace_boat/oak_on"), EveryCompat.res("entity/furnace_boat_mask"))
-//                .addTextureM(modRes("entity/furnace_boat/oak"), EveryCompat.res("entity/furnace_boat_mask"))
                 .build();
         this.addEntry(largeBoats);
 
         furnaceBoats = ItemOnlyEntrySet.builder(WoodType.class, "furnace_boat",
                         getModItem("oak_furnace_boat"),
                         () -> VanillaWoodTypes.OAK,
-                        w -> new FurnaceBoatItem(getBoatType(w))
+                        woodType -> new FurnaceBoatItem(getBoatType(woodType))
                 )
                 .setTabKey(tab)
                 .requiresChildren(VanillaWoodChildKeys.BOAT) //REASON: recipes
                 .addTag(ItemTags.BOATS, Registries.ITEM)
                 .addTag(modRes("furnace_boats"), Registries.ITEM)
                 .addRecipe(modRes("oak_furnace_boat"))
-//                .addTexture(modRes("item/large_oak_boat"))
-//                .addTextureM(modRes("item/oak_furnace_boat"), EveryCompat.res("item/furnace_boat_mask"))
                 .build();
         this.addEntry(furnaceBoats);
     }
 
-    private BoatloadBoatType getBoatType(WoodType w) {
-        return boatTypes.computeIfAbsent(w, k -> BoatloadBoatType.register(new BoatloadBoatType(
-                EveryCompat.res(this.shortenedId() + "/" + w.getAppendableId()),
-                () -> w.getItemOfThis("planks"),
-                () -> w.getItemOfThis("boat"),
-                () -> w.getItemOfThis("chest_boat"),
-                () -> furnaceBoats.items.get(w),
-                () -> largeBoats.items.get(w),
-                w.canBurn(),
+    private BoatloadBoatType getBoatType(WoodType woodType) {
+        return boatTypes.computeIfAbsent(woodType, k -> BoatloadBoatType.register(new BoatloadBoatType(
+                EveryCompat.res(this.shortenedId() + "/" + woodType.getAppendableId()),
+                () -> woodType.getItemOfThis(PLANKS),
+                () -> woodType.getItemOfThis(BOAT),
+                () -> woodType.getItemOfThis(CHEST_BOAT),
+                () -> furnaceBoats.items.get(woodType),
+                () -> largeBoats.items.get(woodType),
+                woodType.canBurn(),
                 false
         )));
     }
@@ -94,13 +91,11 @@ public class BoatLoadModule extends SimpleModule {
                     modRes("item/oak_furnace_boat"), EveryCompat.res("item/furnace_boat_mask"),
                     manager, sink);
 
-            UtilityTexture.addBoatTextureItem(largeBoats,
-                    modRes("entity/large_boat/oak"), null,
-                    manager, sink);
+            UtilityTexture.addBoatTextureEntity(largeBoats,
+                    modRes("entity/large_boat/oak"), null, manager, sink);
 
             UtilityTexture.addBoatTextureItem(largeBoats,
-                    modRes("item/large_oak_boat"), null,
-                    manager, sink);
+                    modRes("item/large_oak_boat"), null, manager, sink);
         });
     }
 
