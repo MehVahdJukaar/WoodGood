8ce8532c70b81aeafb3dd5e5d3ea406a4fccca3a
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-06-09 02:45:34 +0200

    removed unneded 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.bouncycastle.oer.OERDefinition.optional
+
 plugins {
     id("com.possible-triangle.core")
     id("com.possible-triangle.common") apply false
@@ -115,43 +117,3 @@ subprojects {
         }
     }
 }
-
-//DISABLED for now because I think it's using buildAndPublishAll from 1.21.1-oldGradle
-
-//tasks.register("buildAndPublishAll") {
-//    group = "build"
-//    description = "Runs clean, build, publish for all projects"
-//
-//    dependsOn(subprojects.map { it.tasks.named("clean") })
-//    dependsOn(subprojects.map { it.tasks.named("build") })
-//    dependsOn(subprojects.map { it.tasks.named("upload") })
-//
-//    finalizedBy("gitTag")
-//}
-//
-//tasks.register("gitTag") {
-//    group = "build"
-//    description = "Create a tag using mod_version and add the commits"
-//    doLast {
-//        val execOps = serviceOf<ExecOperations>() // Fetches the service
-//        val tag = property("mod_version").toString()
-//        val stdout = ByteArrayOutputStream()
-//
-//        execOps.exec {
-//            commandLine("git", "tag", "-l", tag)
-//            standardOutput = stdout
-//        }
-//
-//        if (!stdout.toString(Charset.defaultCharset()).trim().isEmpty()) {
-//            logger.warn("Git tag '${tag}' already exists")
-//        } else {
-//            execOps.exec {
-//                commandLine("git", "tag", "-a", tag, "-m", "Release $tag")
-//            }
-//            execOps.exec {
-//                commandLine("git", "push", "origin", tag)
-//            }
-//        }
-//    }
-//}
-
