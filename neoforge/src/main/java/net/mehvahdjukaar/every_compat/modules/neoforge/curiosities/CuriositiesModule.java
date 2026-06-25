53537ff7d02de9e51fb581e438b843efa0f3bbd2
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-06-24 20:33:03 -0600

    CuriositiesModule: Updated Recipe Generation to use BambooType recipe

--- neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/curiosities/CuriositiesModule.java
+++ neoforge/src/main/java/net/mehvahdjukaar/every_compat/modules/neoforge/curiosities/CuriositiesModule.java
@@ -5,29 +5,35 @@ import com.teamabnormals.woodworks.common.item.crafting.SawmillRecipe;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.TabAddMode;
-import net.mehvahdjukaar.every_compat.misc.UtilityRecipe;
-import net.mehvahdjukaar.every_compat.misc.UtilityTag;
 import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
-import net.mehvahdjukaar.moonlight.api.resources.RecipeTemplate;
+import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
+import net.mehvahdjukaar.moonlight.api.resources.ResType;
 import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
+import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceSink;
+import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.level.block.Block;
 
 import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
+
+import static net.mehvahdjukaar.every_compat.misc.HardcodedBlockType.IsBambooLike;
+import static net.mehvahdjukaar.every_compat.misc.UtilityTag.getATagOrCreateANew;
+import static net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodChildKeys.PLANKS;
 
 //SUPPORT: v0.1.0+
 public class CuriositiesModule extends EveryCompatModule {
@@ -51,10 +57,8 @@ public class CuriositiesModule extends EveryCompatModule {
                 .setTab(getTab(tabPath))
                 .setTabMode(TabAddMode.AFTER_SAME_WOOD)
                 .defaultRecipe()
-                .addRecipe(modRes("fancied_oak_planks_from_oak_planks_sawing"))
                 .build();
         this.addEntry(fancied_planks);
-
     }
 
     @Override
@@ -64,40 +68,81 @@ public class CuriositiesModule extends EveryCompatModule {
 
         if (PlatHelper.isModLoaded("woodworks")) {
             executor.accept((manager, sink) -> {
-                for (Map.Entry<WoodType, Block> entry : fancied_planks.blocks.entrySet()) {
-                    WoodType woodType = entry.getKey();
-                    Block block = entry.getValue();
+                for (WoodType woodType : fancied_planks.blocks.keySet()) {
 
-                    ResourceLocation sawingLoc = modRes("fancied_oak_planks_from_oak_logs_sawing");
-
-                    var newSawingLoc = sawingLoc.withPrefix(shortenedId() + "/" + woodType.getNamespace() + "/" )
-                            .getPath().replace("oak", woodType.getTypeName());
-
-                    String newTagIng = UtilityTag.getATagOrCreateANew("logs", "stems", woodType, sink, manager).toString();
-
-                    UtilityRecipe.createRecipeWithTag(sawingLoc, EveryCompat.res(newSawingLoc),
-                            "minecraft:oak_logs", newTagIng, block, sink, manager);
+                    createSawmillRecipe(PLANKS, "logs", sink, manager, woodType);
+                    createSawmillRecipe(PLANKS, PLANKS, sink, manager, woodType);
                 }
             });
         }
     }
 
-    @Override
-    public void onModSetup() {
-        super.onModSetup();
+    public void createSawmillRecipe(String typeOutput, String typeInput, ResourceSink sink, ResourceManager manager, WoodType newWoodType) {
+        String fromBambooType = typeInput.matches("logs?") ? "blocks" : typeInput;
 
-        if (PlatHelper.isModLoaded("woodworks")) {
-            RecipeTemplate.register(SawmillRecipe.class, (original, oldBlockType, newBlockType) -> {
-                List<Ingredient> modifiedIngredient = RecipeTemplate.convertIngredients(original.getIngredients(), oldBlockType, newBlockType);
-                Ingredient newInput = Ingredient.of(modifiedIngredient.getFirst().getItems()[0]);
-                ItemStack originalResult = original.getResultItem(RegistryAccess.EMPTY);
-                ItemStack newResult = RecipeTemplate.convertItemStack(originalResult, oldBlockType, newBlockType);
-                if (newResult == null) {
-                    throw new UnsupportedOperationException("Failed to convert recipe result");
-                } else {
-                    return new SawmillRecipe(original.getGroup(), newInput, newResult);
+        ResourceLocation recipeLocation = (IsBambooLike(newWoodType))
+                ? modRes("fancied_bamboo_" + typeOutput + "_from_bamboo_" + fromBambooType + "_sawing")
+                : modRes("fancied_acacia_" + typeOutput + "_from_acacia_" + typeInput + "_sawing");
+
+        if (manager.getResource(ResType.RECIPES.getPath(recipeLocation)).isEmpty()) return;
+
+        Recipe<?> recipe = RPUtils.readRecipe(manager, recipeLocation);
+
+        if (recipe instanceof SawmillRecipe sawmillRecipe) {
+            boolean isIngredientModified = false;
+            Ingredient newIngredient = null;
+
+            var oldIngredients = sawmillRecipe.getIngredients().getFirst().getItems();
+            for (ItemStack oldItemStack : oldIngredients) {
+
+                Item oldItem = oldItemStack.getItem();
+
+                // if the recipe has Items.BARRIER, then it's using TAG as ingredient
+                if (oldItem != Items.BARRIER) {
+                    WoodType oldWoodType = WoodTypeRegistry.INSTANCE.getBlockTypeOf(oldItem);
+                    Item newItem = BlockSetAPI.changeItemType(oldItem, oldWoodType, newWoodType);
+
+                    if (newItem != null) {
+                        newIngredient = Ingredient.of(new ItemStack(newItem));
+                        isIngredientModified = true;
+                    }
                 }
-            });
+                else {
+                    isIngredientModified = true;
+                    ResourceLocation newTag = getATagOrCreateANew("logs", "blocks", newWoodType, sink, manager);
+                    newIngredient = Ingredient.of(TagKey.create(Registries.ITEM, newTag));
+
+                }
+            }
+
+            if (isIngredientModified) {
+                Item oldItem = sawmillRecipe.result.getItem();
+                int count = sawmillRecipe.result.getCount();
+                WoodType oldWoodType = WoodTypeRegistry.INSTANCE.getBlockTypeOf(oldItem);
+
+                if (oldWoodType == VanillaWoodTypes.ACACIA || oldWoodType == VanillaWoodTypes.BAMBOO) {
+                    Item newItem = BlockSetAPI.changeItemType(oldItem, oldWoodType, newWoodType);
+                    if (newItem != null) {
+                        SawmillRecipe newRecipe = new SawmillRecipe(sawmillRecipe.getGroup(), newIngredient, new ItemStack(newItem, count));
+
+                        String appendedPath = recipeLocation.withPrefix(shortenedId() + "/" + newWoodType.getNamespace() + "/").getPath();
+                        String newPath = (oldWoodType == VanillaWoodTypes.ACACIA)
+                                ? appendedPath.replace("acacia", newWoodType.getTypeName())
+                                : appendedPath.replace("bamboo", newWoodType.getTypeName());
+
+                        sink.addRecipe(new RecipeHolder<>(
+                                EveryCompat.res(newPath),
+                                newRecipe)
+                        );
+                    }
+                    else {
+                        EveryCompat.LOGGER.warn("SawmillRecipe - Result cannot be modified for {} - {}", newWoodType.getId(), recipeLocation);
+                    }
+                }
+            }
+            else {
+                EveryCompat.LOGGER.warn("SawmillRecipe - Ingredient cannot modified for {} - {}", newWoodType.getId(), recipeLocation);
+            }
         }
     }
 
