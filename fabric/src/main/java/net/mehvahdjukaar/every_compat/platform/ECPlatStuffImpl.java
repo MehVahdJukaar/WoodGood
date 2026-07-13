af7d2bc3be2883d31610b45418b6143da5f7c36b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-07-13 13:12:52 -0600

    ECPlatStuffImpl(FB): Added StrippableBlockRegistry to a method

--- fabric/src/main/java/net/mehvahdjukaar/every_compat/platform/ECPlatStuffImpl.java
+++ fabric/src/main/java/net/mehvahdjukaar/every_compat/platform/ECPlatStuffImpl.java
@@ -1,5 +1,6 @@
 package net.mehvahdjukaar.every_compat.platform;
 
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -13,7 +14,8 @@ public class ECPlatStuffImpl {
         return stacks;
     }
 
-    public static void registerStripping(Block post, Block stripped) {
+    public static void registerStripping(Block log, Block stripped_log) {
+        StrippableBlockRegistry.register(log, stripped_log);
     }
 
 }
