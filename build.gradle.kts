6a55f9e976975af16e91cebfbd9c0372c0222c50
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-07-15 18:20:53 -0600

    build.gradle.kts(EC): Downgrade Candlelight to v1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.possible-triangle.common") apply false
     id("com.possible-triangle.fabric") apply false
     id("com.possible-triangle.neoforge") apply false
-    id("net.mehvahdjukaar.candlelight") version "1.2.4" apply false
+    id("net.mehvahdjukaar.candlelight") version "1.2.1" apply false
 }
 
 mod {
@@ -24,7 +24,7 @@ subprojects {
     pluginManager.apply("maven-publish")
 
     dependencies {
-        compileOnly("net.mehvahdjukaar:candlelight:1.2.4")
+        compileOnly("net.mehvahdjukaar:candlelight:1.2.1")
     }
 
 
