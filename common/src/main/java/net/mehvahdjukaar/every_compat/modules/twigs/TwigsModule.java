7569ccb6092bf4d1f749fbc1053439e00eb711cb
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-09-11 14:03:55 -0600

    TwigsModule: Updated CreativeTab ID for FABRIC & CLOSED #1318

--- common/src/main/java/net/mehvahdjukaar/every_compat/modules/twigs/TwigsModule.java
+++ common/src/main/java/net/mehvahdjukaar/every_compat/modules/twigs/TwigsModule.java
@@ -6,20 +6,27 @@ import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.TabAddMode;
 import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
+import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 
-//SUPPORT: v3.1.1+
+import java.util.function.Supplier;
+
+///SUPPORT: FABRIC-v4.0.0 | NEOFORGE-v3.1.2+
 public class TwigsModule extends EveryCompatModule {
 
     public final SimpleEntrySet<WoodType, Block> tables;
 
     public TwigsModule(String modId) {
         super(modId, "tw");
+        Supplier<CreativeModeTab> tab = (PlatHelper.getPlatform().isFabric())
+                ? getModTab("item_group")
+                : getModTab("twig");
 
         tables = SimpleEntrySet.builder(WoodType.class, "table",
                         getModBlock("oak_table"), () -> VanillaWoodTypes.OAK,
@@ -28,7 +35,7 @@ public class TwigsModule extends EveryCompatModule {
                 .addTag(modRes("tables"), Registries.ITEM)
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
                 .defaultRecipe()
-                .setTab(getModTab("twig"))
+                .setTab(tab)
                 .setTabMode(TabAddMode.AFTER_SAME_TYPE)
                 .setRenderType(RenderLayer.CUTOUT_MIPPED)
                 .addTexture(modRes("block/oak_table"), PaletteStrategies.PLANKS_REMOVE_DARKEST)
